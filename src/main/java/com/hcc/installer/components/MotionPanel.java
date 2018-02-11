5dbed7d3d8d34e00eb2cfb657a3743438d2b7e30
VRCube <koonjjthebest@gmail.com>
2018-02-11 11:49:49 +0100

    complete installer

---
+++ src/main/java/com/hcc/installer/components/MotionPanel.java
@@ -0,0 +1,60 @@
+/*
+ *     Hypixel Community Client, Client optimized for Hypixel Network
+ *     Copyright (C) 2018  HCC Dev Team
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hcc.installer.components;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+public class MotionPanel extends JPanel {
+    private Point initialClick;
+    private JFrame parent;
+
+    public MotionPanel(final JFrame parent){
+        this.parent = parent;
+
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                initialClick = e.getPoint();
+                getComponentAt(initialClick);
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+                // get location of Window
+                int thisX = parent.getLocation().x;
+                int thisY = parent.getLocation().y;
+
+                // Determine how much the mouse moved since the initial click
+                int xMoved = (thisX + e.getX()) - (thisX + initialClick.x);
+                int yMoved = (thisY + e.getY()) - (thisY + initialClick.y);
+
+                // Move window to this position
+                int X = thisX + xMoved;
+                int Y = thisY + yMoved;
+                parent.setLocation(X, Y);
+            }
+        });
+    }
+}
