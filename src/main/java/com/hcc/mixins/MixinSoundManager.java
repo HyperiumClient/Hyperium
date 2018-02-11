b57d62b0843a9d48732546338f278697a8906a4b
KevinPriv <kb215@ga.newcastle.sch.uk>
2018-02-11 18:13:16 +0000

    Sounds wont play unless minecraft is active

---
+++ src/main/java/com/hcc/mixins/MixinSoundManager.java
@@ -0,0 +1,27 @@
+package com.hcc.mixins;
+
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.SoundManager;
+import org.lwjgl.opengl.Display;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SoundManager.class)
+public class MixinSoundManager {
+
+    /**
+     * Sound will not play unless the window is active
+     *
+     * @param sound
+     * @param ci
+     */
+    @Inject(method = "playSound", at = @At("HEAD"), cancellable = true)
+    private void playSound(ISound sound, CallbackInfo ci) {
+        if (!Display.isActive()) { // todo: settigns to enable / disable
+            ci.cancel();
+        }
+    }
+
+}
