53f3b24ab6f6b3b51a636f4b785c72a58cf09566
KevinPriv <kb215@ga.newcastle.sch.uk>
2018-02-14 21:01:06 +0000

    Fixed Sk1ers shit code part #3

--- src/main/java/com/hcc/handlers/handlers/HypixelDetector.java
+++ src/main/java/com/hcc/handlers/handlers/HypixelDetector.java
@@ -1,41 +1,26 @@
 package com.hcc.handlers.handlers;
 
 import com.hcc.event.InvokeEvent;
-import com.hcc.event.TickEvent;
-import me.semx11.autotip.util.ReflectionUtil;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiPlayerTabOverlay;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.IChatComponent;
+import com.hcc.event.ServerJoinEvent;
+import com.hcc.event.ServerLeaveEvent;
 
-import java.lang.reflect.Field;
+import java.util.regex.Pattern;
 
 public class HypixelDetector {
-    private static final String HYPIXEL_HEADER = "You are playing on MC.HYPIXEL.NET";
-    private static final Field HEADER_FIELD = ReflectionUtil.findField(GuiPlayerTabOverlay.class, "field_175256_i", "header");
+
+    private static final Pattern HYPIXEL_PATTERN =
+            Pattern.compile("^(?:(?:(?:\\w+\\.)?hypixel\\.net)|(?:209\\.222\\.115\\.\\d{1,3}))(?::\\d{1,5})?$", Pattern.CASE_INSENSITIVE);
+
     private boolean hypixel = false;
 
-    public HypixelDetector() {
-
-    }
-
-    public static Object getHeader() {
-        try {
-            return HEADER_FIELD.get(Minecraft.getMinecraft().ingameGUI.getTabList());
-        } catch (IllegalAccessException | NullPointerException e) {
-            return null;
-        }
+    @InvokeEvent
+    public void serverJoinEvent(ServerJoinEvent event) {
+        hypixel = HYPIXEL_PATTERN.matcher(event.getServer()).find();
     }
 
     @InvokeEvent
-    public void tick(TickEvent event) {
-
-        //Check for Hypixel every tick to make sure bad things don't happen
-        Object header = getHeader();
-        if (header != null) {
-            String text = EnumChatFormatting.getTextWithoutFormattingCodes(((IChatComponent) header).getUnformattedText());
-            hypixel = text.equalsIgnoreCase(HYPIXEL_HEADER);
-        } else hypixel = false;
+    public void serverLeaveEvent(ServerLeaveEvent event) {
+        hypixel = false;
     }
 
     public boolean isHypixel() {
