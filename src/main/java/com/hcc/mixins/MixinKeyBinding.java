443ad2279c7c798fe2b47f48fd1c235555947fec
KevinPriv <kb215@ga.newcastle.sch.uk>
2018-02-15 16:51:09 +0000

    Added ToggleSprint

---
+++ src/main/java/com/hcc/mixins/MixinKeyBinding.java
@@ -0,0 +1,13 @@
+package com.hcc.mixins;
+
+import net.minecraft.client.settings.KeyBinding;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(KeyBinding.class)
+public interface MixinKeyBinding {
+
+    @Accessor
+    void setPressed(boolean pressed);
+
+}
