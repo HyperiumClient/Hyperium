1bb3b50a6b91e74c1c229cbde00a9affa16dd8ad
VRCube <vrcproxz@gmail.com>
2018-02-19 15:29:59 +0100

    created port detector

--- src/main/java/com/hcc/integrations/spotify/Spotify.java
+++ src/main/java/com/hcc/integrations/spotify/Spotify.java
@@ -18,6 +18,7 @@
 
 package com.hcc.integrations.spotify;
 
+import com.hcc.integrations.os.OsHelper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -31,10 +32,16 @@ import java.util.Objects;
 
 public class Spotify {
     private OkHttpClient client = new OkHttpClient();
-    public Spotify() throws Exception{
-        File webHelper = getWebHelper();
-        if(webHelper == null)
+    private final int START_HTTPS_PORT = 4370;
+    private final int END_HTTPS_PORT = 4379;
+    private final int START_HTTP_PORT = 4380;
+    private final int END_HTTP_PORT = 4389;
+    private int localPort = START_HTTPS_PORT;
+
+    public Spotify() throws Exception {
+        if (getWebHelper() == null)
             throw new UnsupportedOperationException("Could not find WebHelper // OS not supported!");
+        startWebHelper();
     }
 
     /**
@@ -46,12 +53,41 @@ public class Spotify {
         Request request = new Request.Builder()
                 .url(url)
                 .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+                .addHeader("Origin", "https://open.spotify.com")
                 .build();
         Response response = client.newCall(request).execute();
         return new JSONObject(Objects.requireNonNull(response.body()).string());
     }
 
-    private void statWebHelper(){
+    private int detectPort() throws IOException {
+        try {
+            get(genSpotifyUrl("/service/version.json?service=remote"));
+        } catch (IOException e) {
+            if (localPort == END_HTTP_PORT) {
+                throw e;
+            } else if (localPort == END_HTTPS_PORT) {
+                localPort = START_HTTP_PORT;
+            } else {
+                ++localPort;
+            }
+            return this.detectPort();
+        }
+        return localPort;
+    }
+
+    private String genSpotifyUrl(String path) {
+        String protocol = "https://";
+        if (localPort >= START_HTTP_PORT && localPort <= END_HTTP_PORT) {
+            protocol = "http://";
+        }
+        return String.format("%s%s:%d%s", protocol, "127.0.0.1", localPort, path);
+    }
+
+    /**
+     * starts spotify's webHelper
+     */
+    private void startWebHelper() {
+        if (isWebHelperRunning()) return;
         try {
             Desktop.getDesktop().open(Objects.requireNonNull(getWebHelper()));
         } catch (IOException e) {
@@ -62,13 +98,22 @@ public class Spotify {
     /**
      * @return web helper executable path
      */
-    private File getWebHelper(){
-        if(SystemUtils.IS_OS_WINDOWS){
+    private File getWebHelper() {
+        if (SystemUtils.IS_OS_WINDOWS) {
             return new File(System.getProperty("user.home"), "\\AppData\\Roaming\\Spotify\\SpotifyWebHelper.exe");
-        }else if(SystemUtils.IS_OS_MAC){
-            return new File(System.getProperty("user.home"),"/Library/Application Support/Spotify/SpotifyWebHelper");
-        }else{
+        } else if (SystemUtils.IS_OS_MAC) {
+            return new File(System.getProperty("user.home"), "/Library/Application Support/Spotify/SpotifyWebHelper");
+        } else {
             return null;
         }
+    }
+
+    private boolean isWebHelperRunning() {
+        try {
+            return OsHelper.isProcessRunning("SpotifyWebHelper");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
