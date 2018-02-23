36779143f5229992d51f1d74b41d18bf21a75b26
boomboompower <boomboompower@users.noreply.github.com>
2018-02-24 02:59:08 +1100

    Cleanup & TimeChanger (#35)
    * Remove class files
    
    * TimeChanger
    
    * lowercase :)
    
    * Fix SkinChangeer & message separators

---
+++ src/main/java/cc/hyperium/mods/timechanger/TimeChanger.java
@@ -0,0 +1,76 @@
+package cc.hyperium.mods.timechanger;
+
+import cc.hyperium.Hyperium;
+import cc.hyperium.event.EventBus;
+import cc.hyperium.event.InvokeEvent;
+import cc.hyperium.event.TickEvent;
+import cc.hyperium.mods.IBaseMod;
+import cc.hyperium.mods.timechanger.commands.CommandTimeChangerDay;
+import cc.hyperium.mods.timechanger.commands.CommandTimeChangerFastTime;
+import cc.hyperium.mods.timechanger.commands.CommandTimeChangerNight;
+import cc.hyperium.mods.timechanger.commands.CommandTimeChangerReset;
+import cc.hyperium.mods.timechanger.commands.CommandTimeChangerSunset;
+import cc.hyperium.utils.ChatColor;
+import net.minecraft.client.Minecraft;
+
+public class TimeChanger extends IBaseMod {
+    
+    private final Metadata metadata = new Metadata(this, "timechanger", "1.0", "fyu");
+    private final Minecraft mc = Minecraft.getMinecraft();
+    
+    private double fastTimeMultiplier = 1.0D;
+    private TimeType timeType = TimeType.VANILLA;
+    
+    public TimeChanger() {
+        this.metadata.setDisplayName(ChatColor.BLUE + "TimeChanger");
+    }
+    
+    @Override
+    public IBaseMod init() {
+        Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().registerCommand(new CommandTimeChangerDay(this));
+        Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().registerCommand(new CommandTimeChangerNight(this));
+        Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().registerCommand(new CommandTimeChangerSunset(this));
+        Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().registerCommand(new CommandTimeChangerReset(this));
+        Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().registerCommand(new CommandTimeChangerFastTime(this));
+    
+        EventBus.INSTANCE.register(this);
+        
+        return this;
+    }
+    
+    @Override
+    public Metadata getModMetadata() {
+        return this.metadata;
+    }
+    
+    public TimeType getTimeType() {
+        return this.timeType;
+    }
+    
+    public void setTimeType(TimeType timeType) {
+        this.timeType = timeType;
+    }
+    
+    public double getFastTimeMutliplier() {
+        return this.fastTimeMultiplier;
+    }
+    
+    public void setFastTimeMultiplier(double fastTimeMultiplier) {
+        this.fastTimeMultiplier = fastTimeMultiplier;
+    }
+    
+    @InvokeEvent
+    public void onTick(TickEvent event) {
+        if (this.mc.theWorld != null && this.timeType == TimeType.FAST) {
+            this.mc.theWorld.setWorldTime((long) (System.currentTimeMillis() * this.fastTimeMultiplier % 24000.0));
+        }
+    }
+    
+    public enum TimeType {
+        DAY, // During the day
+        SUNSET, // Just before night
+        NIGHT, // Always night
+        VANILLA, // The normal time of the game
+        FAST // Sped up vanilla time
+    }
+}
