4e7b143d75a660e4baee41167cc008c5a93d379a
boomboompower <boomboompowermc@gmail.com>
2018-02-26 16:51:39 +1100

    Something Shiny

--- src/main/java/cc/hyperium/gui/settings/items/GeneralSetting.java
+++ src/main/java/cc/hyperium/gui/settings/items/GeneralSetting.java
@@ -49,6 +49,8 @@ public class GeneralSetting extends SettingGui {
     @ConfigOpt
     public static boolean fastchatEnabled = false;
     @ConfigOpt
+    public static boolean shinyPotsEnabled = false;
+    @ConfigOpt
     public static String menuStyle = GuiStyle.DEFAULT.toString();
 
     private SelectionItem<String> discordRP;
@@ -66,6 +68,8 @@ public class GeneralSetting extends SettingGui {
     private SelectionItem<String> framerateLimiter;
 
     private SelectionItem<String> fastChat;
+    
+    private SelectionItem<String> shinyPots;
 
     private SelectionItem<String> menuStyleSelection;
 
@@ -134,7 +138,15 @@ public class GeneralSetting extends SettingGui {
         }));
         fastChat.addDefaultOnOff();
         fastChat.setSelectedItem(fastchatEnabled ? "ON" : "OFF");
-        settingItems.add(menuStyleSelection = new SelectionItem(8, getX(), getDefaultItemY(8),  width - getX() * 2, "MENU STYLE", i->{
+    
+        settingItems.add(shinyPots = new SelectionItem(8, getX(), getDefaultItemY(8),  width - getX() * 2, "SHINY POTS", i-> {
+            ((SelectionItem)i).nextItem();
+            shinyPotsEnabled = ((SelectionItem) i).getSelectedItem().equals("ON");
+        }));
+        shinyPots.addDefaultOnOff();
+        shinyPots.setSelectedItem(shinyPotsEnabled ? "ON" : "OFF");
+        
+        settingItems.add(menuStyleSelection = new SelectionItem(9, getX(), getDefaultItemY(9),  width - getX() * 2, "MENU STYLE", i->{
             ((SelectionItem)i).nextItem();
             menuStyle = (String)((SelectionItem)i).getSelectedItem();
         }));
