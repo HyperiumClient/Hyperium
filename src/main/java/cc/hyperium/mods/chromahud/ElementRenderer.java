60414c2828c76e6b0afd127bda3db8abf1f15c41
Mitchell Katz <sk1er@lythrim.net>
2018-02-28 08:58:21 -0500

    Made render hud event under tab

--- src/main/java/cc/hyperium/mods/chromahud/ElementRenderer.java
+++ src/main/java/cc/hyperium/mods/chromahud/ElementRenderer.java
@@ -20,7 +20,7 @@ package cc.hyperium.mods.chromahud;
 
 import cc.hyperium.Hyperium;
 import cc.hyperium.event.InvokeEvent;
-import cc.hyperium.event.RenderEvent;
+import cc.hyperium.event.RenderHUDEvent;
 import cc.hyperium.event.TickEvent;
 import cc.hyperium.utils.RenderUtils;
 import net.minecraft.client.Minecraft;
@@ -104,7 +104,7 @@ public class ElementRenderer {
                     ? fontRendererObj.getStringWidth(string)
                     : 0;
             if (current.isHighlighted()) {
-                if(current.getDisplayItems().stream().anyMatch(i -> i.getType().contains("CB"))){
+                if (current.getDisplayItems().stream().anyMatch(i -> i.getType().contains("CB"))) {
                     RenderUtils.drawRect((int) ((tx - 1) / getCurrentScale() - shift), (int) ((ty - 2) / getCurrentScale()), (int) ((tx + 1) / getCurrentScale()) + 50 - shift, (int) ((ty + 2) / getCurrentScale()) + 8, new Color(0, 0, 0, 120).getRGB());
                 } else {
                     int stringWidth = fontRendererObj.getStringWidth(string);
@@ -113,12 +113,12 @@ public class ElementRenderer {
                 }
             }
             if (current.isChroma()) {
-                if(current.getDisplayItems().stream().anyMatch(i -> i.getType().contains("CB")))
+                if (current.getDisplayItems().stream().anyMatch(i -> i.getType().contains("CB")))
                     drawChromaString(string, ((50 - fontRendererObj.getStringWidth(string)) / 2) + (tx - shift), (int) ty);
                 else
                     drawChromaString(string, tx - shift, (int) ty);
             } else {
-                if(current.getDisplayItems().stream().anyMatch(i -> i.getType().contains("CB")))
+                if (current.getDisplayItems().stream().anyMatch(i -> i.getType().contains("CB")))
                     fontRendererObj.drawString(string, (float) (((50 - fontRendererObj.getStringWidth(string)) / 2) + (tx / getCurrentScale() - shift)), (int) (ty / getCurrentScale()), getColor(color, x), current.isShadow());
                 else
                     fontRendererObj.drawString(string, (int) (tx / getCurrentScale() - shift), (int) (ty / getCurrentScale()), getColor(color, x), current.isShadow());
@@ -198,6 +198,7 @@ public class ElementRenderer {
         GlStateManager.scale(1.0 / element.getScale(), 1.0 / element.getScale(), 0);
     }
 
+
     public static FontRenderer getFontRenderer() {
         return fontRendererObj;
     }
@@ -214,12 +215,17 @@ public class ElementRenderer {
     }
 
     @InvokeEvent
-    public void onRenderTick(RenderEvent event) {
+    public void onRenderTick(RenderHUDEvent event) {
         resolution = new ScaledResolution(Minecraft.getMinecraft());
         if (!this.minecraft.inGameHasFocus || this.minecraft.gameSettings.showDebugInfo) {
             return;
         }
+//        GlStateManager.color(1.0F,1.0F,1.0F,1.0F);
+
         renderElements();
+
+//        GlStateManager.color(1.0F,1.0F,1.0F,1.0F);
+
     }
 
     public void renderElements() {
@@ -237,6 +243,7 @@ public class ElementRenderer {
             }
         }
 
+        GlStateManager.color(1.0F,1.0F,1.0F,1.0F);
 
         List<DisplayElement> elementList = mod.getDisplayElements();
         for (DisplayElement element : elementList) {
