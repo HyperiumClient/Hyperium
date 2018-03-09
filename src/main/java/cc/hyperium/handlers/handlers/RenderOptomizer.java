5144c7f00e7a106dda0c47a65b306ce31e778886
Mitchell Katz <sk1er@lythrim.net>
2018-03-08 23:33:02 -0500

    Limit duplicate armour stand feature for more performance

---
+++ src/main/java/cc/hyperium/handlers/handlers/RenderOptomizer.java
@@ -0,0 +1,39 @@
+package cc.hyperium.handlers.handlers;
+
+import cc.hyperium.config.ConfigOpt;
+import cc.hyperium.event.InvokeEvent;
+import cc.hyperium.event.RenderHUDEvent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.util.EnumChatFormatting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RenderOptomizer {
+
+    private List<String> rendered = new ArrayList<>();
+
+    @ConfigOpt
+    private boolean limitArmourStands = true;
+
+    @InvokeEvent
+    public void renderHUD(RenderHUDEvent event) {
+        rendered.clear();
+    }
+
+    public boolean shouldRender(Entity entity) {
+        if (entity instanceof EntityArmorStand) {
+
+            String textWithoutFormattingCodes = EnumChatFormatting.getTextWithoutFormattingCodes(entity.getDisplayName().getUnformattedText());
+            boolean flag = true;
+            if (rendered.contains(textWithoutFormattingCodes)) {
+                flag = false;
+            } else rendered.add(textWithoutFormattingCodes);
+            return flag;
+
+
+        }
+        return true;
+    }
+}
