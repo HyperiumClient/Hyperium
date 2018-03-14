0230369229c770a512bb47d1f76d55a825b319a0
VRCube <vrcproxz@gmail.com>
2018-03-14 18:47:09 +0100

    splash screen background

--- src/main/java/cc/hyperium/mixins/MixinMinecraft.java
+++ src/main/java/cc/hyperium/mixins/MixinMinecraft.java
@@ -178,9 +178,11 @@ import net.minecraft.client.audio.SoundHandler;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.*;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.resources.DefaultResourcePack;
 import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Timer;
 import net.minecraft.util.Util;
@@ -466,16 +468,36 @@ public abstract class MixinMinecraft {
      * change to splash screen logo
      * @author Cubxity
      */
-    @Inject(method = "drawSplashScreen", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/texture/TextureManager.bindTexture(Lnet/minecraft/util/ResourceLocation;)V", shift = At.Shift.AFTER))
-    private void overrideLogo(TextureManager tm, CallbackInfo ci){
+    @Overwrite
+    private void drawSplashScreen(TextureManager tm){
+        ScaledResolution scaledresolution = new ScaledResolution(Minecraft.getMinecraft());
+        int i = scaledresolution.getScaleFactor();
+        Framebuffer framebuffer = new Framebuffer(scaledresolution.getScaledWidth() * i, scaledresolution.getScaledHeight() * i, true);
+        framebuffer.bindFramebuffer(false);
+        GlStateManager.matrixMode(5889);
+        GlStateManager.loadIdentity();
+        GlStateManager.ortho(0.0D, (double)scaledresolution.getScaledWidth(), (double)scaledresolution.getScaledHeight(), 0.0D, 1000.0D, 3000.0D);
+        GlStateManager.matrixMode(5888);
+        GlStateManager.loadIdentity();
+        GlStateManager.translate(0.0F, 0.0F, -2000.0F);
+        GlStateManager.disableLighting();
+        GlStateManager.disableFog();
+        GlStateManager.disableDepth();
+        GlStateManager.enableTexture2D();
         if(splash == null)
             splash = new ResourceLocation("textures/hyperium-splash.png");
         tm.bindTexture(splash);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        Gui.drawScaledCustomSizeModalRect(0, 0, 0, 0, 1920, 1080, scaledresolution.getScaledWidth(), scaledresolution.getScaledHeight(), 1920, 1080);
+        drawProgress();
+        framebuffer.unbindFramebuffer();
+        framebuffer.framebufferRender(scaledresolution.getScaledWidth() * i, scaledresolution.getScaledHeight() * i);
+        GlStateManager.enableAlpha();
+        GlStateManager.alphaFunc(516, 0.1F);
+        this.updateDisplay();
     }
 
-    @Inject(method = "drawSplashScreen", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/GlStateManager.disableFog()V", shift = At.Shift.AFTER))
-    private void drawProgress(TextureManager textureManagerInstance, CallbackInfo ci){
-        if(sfr == null)
-            sfr = new FontRenderer(Minecraft.getMinecraft().gameSettings, new net.minecraft.util.ResourceLocation("textures/font/ascii.png"), textureManagerInstance, false);
+    private void drawProgress(){
+
     }
 }
