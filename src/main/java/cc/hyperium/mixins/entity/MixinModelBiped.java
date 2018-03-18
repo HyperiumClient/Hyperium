8e26c08eb9bb1805c9f7829367a75671b051e525
CoalCoding <coalores@gmail.com>
2018-03-18 15:42:04 +0000

    Added basic button scaling to main menu and optimized injection.

--- src/main/java/cc/hyperium/mixins/entity/MixinModelBiped.java
+++ src/main/java/cc/hyperium/mixins/entity/MixinModelBiped.java
@@ -172,7 +172,6 @@ import cc.hyperium.event.EntityRenderEvent;
 import cc.hyperium.event.EventBus;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -182,10 +181,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinModelBiped {
 
     @Inject(method = "render", at = @At("HEAD") ,cancellable = true)
-    private void render(Entity entityIn, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float scale, CallbackInfo ci)
+    private void render(Entity entityIn, float limbSwing, float limbSwingAmount, float p_78088_4_, float ageInTicks, float headPitch, float scale, CallbackInfo ci)
     {
         ModelBiped model = (ModelBiped) (Object) this;
-        EntityRenderEvent event = new EntityRenderEvent(entityIn, model, p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, scale);
+        EntityRenderEvent event = new EntityRenderEvent(entityIn, model, limbSwing, limbSwingAmount, p_78088_4_, ageInTicks, headPitch, scale);
         EventBus.INSTANCE.post(event);
         if(event.isCancelled()) {
             ci.cancel();
