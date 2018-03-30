56145bffbc702f3df06fd0075b49602c4e65b666
CoalCoding <coalores@gmail.com>
2018-03-30 19:08:42 +0100

    Fixed CPS and FastChat bugs, removed friend request example.

--- src/main/java/cc/hyperium/mixins/gui/MixinGuiNewChat.java
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiNewChat.java
@@ -20,6 +20,7 @@ package cc.hyperium.mixins.gui;
 import cc.hyperium.event.ChatEvent;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.gui.settings.items.BackgroundSettings;
+import cc.hyperium.gui.settings.items.GeneralSetting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ChatLine;
 import net.minecraft.client.gui.Gui;
@@ -150,6 +151,7 @@ public abstract class MixinGuiNewChat {
                             {
                                 int i2 = 0;
                                 int j2 = -i1 * 9;
+                                System.out.println("FASTCHAT: " + GeneralSetting.fastchatEnabled);
                                 if(!BackgroundSettings.fastChatEnabled) {
                                     Gui.drawRect(i2, j2 - 9, i2 + l + 4, j2, l1 / 2 << 24);
                                 }
