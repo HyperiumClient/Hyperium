deee145306874569fc2cc855937caad14c6ef55f
Amplifiable2 <37980280+Amplifiable2@users.noreply.github.com>
2018-04-02 19:30:15 +0200

    oops unused import leftover from debug

--- src/main/java/cc/hyperium/Hyperium.java
+++ src/main/java/cc/hyperium/Hyperium.java
@@ -49,7 +49,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.Display;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 
