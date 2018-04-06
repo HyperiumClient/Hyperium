93424c364659f89f6abe65c04f5ff2ed663c3da9
VRCube <vrcproxz@gmail.com>
2018-04-06 15:42:03 +0200

    Fixed codesource

--- src/main/java/cc/hyperium/Hyperium.java
+++ src/main/java/cc/hyperium/Hyperium.java
@@ -261,7 +261,7 @@ public class Hyperium {
         if (updateQueue) {
             try {
                 boolean windows = InstallerFrame.OsCheck.getOperatingSystemType() == InstallerFrame.OsCheck.OSType.Windows;
-                String cs = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getFile()).getAbsolutePath();
+                String cs = new File(getClass().getResource('/' + getClass().getName().replace('.', '/') + ".class").getFile()).getAbsolutePath();
                 System.out.println("cs="+cs);
                 Runtime.getRuntime().exec(new String[]{
                         windows ? "cmd" : "bash",
