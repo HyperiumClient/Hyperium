e9b516f7d9776b42b1a0245ea5c1b60f0d0e0927
Hunter <thefalsehonesty@gmail.com>
2018-04-08 16:33:17 -0400

    Made dab animation more fluid

--- src/main/java/cc/hyperium/handlers/handlers/DabHandler.java
+++ src/main/java/cc/hyperium/handlers/handlers/DabHandler.java
@@ -1,8 +1,8 @@
 package cc.hyperium.handlers.handlers;
 
 import cc.hyperium.config.ConfigOpt;
-import cc.hyperium.event.InvokeEvent;
-import cc.hyperium.event.TickEvent;
+import cc.hyperium.gui.HyperiumGui;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelPlayer;
@@ -12,30 +12,35 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class DabHandler {
 
-
     @ConfigOpt
-    public int speed = 8;
+    public float speed = 7;
     private int dabs;
     private ConcurrentHashMap<UUID, DabState> dabStates = new ConcurrentHashMap<>();
     private float state = 0;
     private boolean right = true;
     private boolean asc = true;
 
-    @InvokeEvent
-    public void tick(TickEvent event) {
-        dabStates.values().forEach(DabState::tick);
-        speed = 20;
-        state = state + (asc ? speed : -speed);
+    public void update() {
+        dabStates.values().forEach(DabState::update);
+        speed = 7;
+
+        state = HyperiumGui.clamp(
+                HyperiumGui.easeOut(
+                        this.state,
+                        this.asc ? 100.0f : 0.0f,
+                        0.1f,
+                        speed
+                ),
+                0.0f,
+                100.0f
+        );
+
         if (state <= 0) {
-            state = 0;
             asc = true;
             right = !right;
-        }
-        if (state >= 100) {
-            state = 100;
+        } else if (state >= 100) {
             asc = false;
         }
-
     }
 
     public int getDabs() {
@@ -59,8 +64,10 @@ public class DabHandler {
     }
 
     public void modify(AbstractClientPlayer entity, ModelPlayer player) {
-        int ticks = get(entity.getUniqueID()).dabTicks;
-        if (ticks != 0) {
+        update();
+
+        int ticks = get(entity.getUniqueID()).dabFrames;
+        if (ticks > 0) {
             float heldPercent = state / 100F;
             if (right) {
                 player.bipedRightArm.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
@@ -73,12 +80,14 @@ public class DabHandler {
                 player.bipedLeftArmwear.rotateAngleX = (float) Math.toRadians(15.0f * heldPercent);
                 player.bipedLeftArmwear.rotateAngleY = (float) Math.toRadians(15.0f * heldPercent);
                 player.bipedLeftArmwear.rotateAngleZ = (float) Math.toRadians(-110.0f * heldPercent);
-                final float rotationX = entity.rotationPitch;
-                player.bipedHead.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
-                final float rotationY = entity.renderYawOffset - entity.rotationYaw;
-                player.bipedHead.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
+//                final float rotationX = entity.rotationPitch;
+//                player.bipedHead.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
+//                final float rotationY = entity.renderYawOffset - entity.rotationYaw;
+//                player.bipedHead.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
                 player.bipedHeadwear.rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
                 player.bipedHeadwear.rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
+                player.bipedHead.rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
+                player.bipedHead.rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
             } else {
                 player.bipedLeftArm.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
                 player.bipedLeftArm.rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
@@ -90,19 +99,21 @@ public class DabHandler {
                 player.bipedRightArmwear.rotateAngleX = (float) Math.toRadians(-15.0f * heldPercent);
                 player.bipedRightArmwear.rotateAngleY = (float) Math.toRadians(-15.0f * heldPercent);
                 player.bipedRightArmwear.rotateAngleZ = (float) Math.toRadians(110.0f * heldPercent);
-                final float rotationX = entity.rotationPitch;
-                player.bipedHead.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
-                final float rotationY = entity.renderYawOffset - entity.rotationYaw;
-                player.bipedHead.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
+//                final float rotationX = entity.rotationPitch;
+//                player.bipedHead.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
+//                final float rotationY = entity.renderYawOffset - entity.rotationYaw;
+//                player.bipedHead.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
                 player.bipedHeadwear.rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
                 player.bipedHeadwear.rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
+                player.bipedHead.rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
+                player.bipedHead.rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
             }
 
 
         }
     }
     public void modify(AbstractClientPlayer entity, ModelBiped player) {
-        int ticks = get(entity.getUniqueID()).dabTicks;
+        int ticks = get(entity.getUniqueID()).dabFrames;
         if (ticks != 0) {
             float heldPercent = state / 100F;
             if (right) {
@@ -136,26 +147,32 @@ public class DabHandler {
     }
     public class DabState {
         UUID uuid;
-        int dabTicks = 0;
+        int dabFrames = 0;
+        long systemTime;
 
         public DabState(UUID uuid) {
             this.uuid = uuid;
+            this.systemTime = Minecraft.getSystemTime();
         }
 
-        void tick() {
-            if (dabTicks > 0)
-                dabTicks--;
+        void update() {
+            while (this.systemTime < Minecraft.getSystemTime() + (1000 / 60)) {
+                this.dabFrames--;
+                this.systemTime += (1000 / 60);
+            }
+
+            if (dabFrames < 0) dabFrames = 0;
         }
 
         public void ensureDabbingFor(int seconds) {
-            seconds *= 20;
+            seconds *= 60;
 
-            dabTicks = Math.max(dabTicks, seconds);
+            dabFrames = Math.max(dabFrames, seconds);
 
         }
 
         public void stopDabbing() {
-            dabTicks = 0;
+            dabFrames = 0;
         }
 
     }
