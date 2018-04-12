3a67dd48ccc3f92435b27375d5d6130873fe56ba
Amplifiable2 <gerritegberink3@gmail.com>
2018-04-12 18:18:44 +0200

    Block + break block at the same time (not bannable, doesn't actually break block)

---
+++ src/main/java/cc/hyperium/mods/oldanimations/OldAnimations.java
@@ -0,0 +1,25 @@
+package cc.hyperium.mods.oldanimations;
+
+import cc.hyperium.event.EventBus;
+import cc.hyperium.mods.AbstractMod;
+import cc.hyperium.utils.ChatColor;
+
+public class OldAnimations extends AbstractMod {
+
+    private final Metadata metadata;
+    public OldAnimations() {
+        Metadata data = new Metadata(this, "OldAnimations", "1.0", "Amplifiable");
+        data.setDisplayName(ChatColor.AQUA + "OldAnimations");
+        this.metadata = data;
+    }
+    @Override
+    public AbstractMod init() {
+        EventBus.INSTANCE.register(new AnimationEventHandler());
+        return this;
+    }
+
+    @Override
+    public Metadata getModMetadata() {
+        return this.metadata;
+    }
+}
