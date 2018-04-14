24300f6036f578b4d08ae73f1cc92df08b0f6f87
Simcha van Collem <simcha@vancollem.nl>
2018-04-14 16:15:05 +0200

    renamed MixinDabRender.java from MixingDabRender.java

--- src/main/java/cc/hyperium/mixins/renderer/MixinDabRender.java
+++ src/main/java/cc/hyperium/mixins/renderer/MixinDabRender.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ModelBiped.class)
-public class MixingDabRender extends ModelBase {
+public class MixinDabRender extends ModelBase {
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;pushMatrix()V"))
     public void render(Entity entityIn, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float scale, CallbackInfo i) {
