f1effecc9a3a698058b404b7eb4131acac023e72
Amplifiable2 <gerritegberink3@gmail.com>
2018-04-19 08:33:37 +0200

    codacy shut up

--- src/main/kotlin/cc/hyperium/event/events.kt
+++ src/main/kotlin/cc/hyperium/event/events.kt
@@ -208,4 +208,7 @@ class EntityRenderEvent(val entityIn: Entity,
                         val p_78088_3_: Float, val p_78088_4_: Float,
                         val p_78088_5_: Float, val p_78088_6_: Float, val scale: Float): CancellableEvent()
 
+/**
+ * Invoked when the scoreboard is rendered
+ */
 class RenderScoreboardEvent(val x: Double, val y: Double): CancellableEvent()
