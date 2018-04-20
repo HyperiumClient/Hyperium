bd371592006cca0eea9bf3f34ca61bdb7af9a567
Amplifiable2 <37980280+Amplifiable2@users.noreply.github.com>
2018-04-20 20:26:45 +0200

    part 2

--- src/main/kotlin/cc/hyperium/internal/addons/strategy/WorkspaceAddonLoader.kt
+++ src/main/kotlin/cc/hyperium/internal/addons/strategy/WorkspaceAddonLoader.kt
@@ -1,13 +1,10 @@
 package cc.hyperium.internal.addons.strategy
 
+import cc.hyperium.internal.addons.AddonBootstrap
 import cc.hyperium.internal.addons.AddonManifest
 import cc.hyperium.internal.addons.misc.AddonManifestParser
-import net.minecraft.client.Minecraft
-import net.minecraft.client.resources.FolderResourcePack
-import net.minecraft.client.resources.IResourcePack
 import org.apache.commons.io.IOUtils
 import java.io.File
-import java.lang.reflect.Field
 import java.nio.charset.Charset
 
 /**
@@ -26,20 +23,9 @@ class WorkspaceAddonLoader : AddonLoaderStrategy() {
     @Throws(Exception::class)
     override fun load(file: File?): AddonManifest? {
         val resource = javaClass.classLoader.getResource("addon.json") ?: return null // not in workspace
-        val resourcePack = FolderResourcePack(file)
-        var defaultResourcePacksField: Field
-        try {
-            defaultResourcePacksField = Minecraft.getMinecraft().javaClass.getDeclaredField("aA")
-        } catch (e: NoSuchFieldException) {
-            defaultResourcePacksField = Minecraft.getMinecraft().javaClass.getDeclaredField("defaultResourcePacks")
+        if (javaClass.classLoader.getResource("pack.mcmeta") != null) {
+            AddonBootstrap.addonResourcePacks.plus(file)
         }
-
-        defaultResourcePacksField.isAccessible = true
-        var packs: List<IResourcePack> = defaultResourcePacksField.get(Minecraft.getMinecraft()) as List<IResourcePack>
-
-        packs.plus(resourcePack)
-
-        defaultResourcePacksField.set(Minecraft.getMinecraft(), packs)
 
         val lines = IOUtils.toString(resource.openStream(), Charset.defaultCharset())
 
