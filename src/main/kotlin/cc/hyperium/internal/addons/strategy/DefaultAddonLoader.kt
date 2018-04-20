9b6a8e6b4628667fb92eb8b12b48ba9fc386a81d
Amplifiable2 <37980280+Amplifiable2@users.noreply.github.com>
2018-04-20 20:25:57 +0200

    part 1

--- src/main/kotlin/cc/hyperium/internal/addons/strategy/DefaultAddonLoader.kt
+++ src/main/kotlin/cc/hyperium/internal/addons/strategy/DefaultAddonLoader.kt
@@ -1,16 +1,14 @@
 package cc.hyperium.internal.addons.strategy
 
+import cc.hyperium.internal.addons.AddonBootstrap
 import cc.hyperium.internal.addons.AddonManifest
 import cc.hyperium.internal.addons.misc.AddonLoadException
 import cc.hyperium.internal.addons.misc.AddonManifestParser
-import net.minecraft.client.Minecraft
 import net.minecraft.client.resources.FileResourcePack
-import net.minecraft.client.resources.FolderResourcePack
 import net.minecraft.client.resources.IResourcePack
 import net.minecraft.launchwrapper.Launch
 
 import java.io.File
-import java.lang.reflect.Field
 import java.util.jar.JarFile
 
 /**
@@ -36,23 +34,10 @@ class DefaultAddonLoader : AddonLoaderStrategy() {
         val jar = JarFile(file)
         val manifest = AddonManifestParser(jar).getAddonManifest()
 
+        AddonBootstrap.addonResourcePacks.add(file)
+
         val uri = file.toURI()
         Launch.classLoader.addURL(uri.toURL())
-
-        val resourcePack = FileResourcePack(file)
-        var defaultResourcePacksField: Field
-        try {
-            defaultResourcePacksField = Minecraft.getMinecraft().javaClass.getDeclaredField("aA")
-        } catch (e: NoSuchFieldException) {
-            defaultResourcePacksField = Minecraft.getMinecraft().javaClass.getDeclaredField("defaultResourcePacks")
-        }
-
-        defaultResourcePacksField.isAccessible = true
-        var packs: List<IResourcePack> = defaultResourcePacksField.get(Minecraft.getMinecraft()) as List<IResourcePack>
-
-        packs.plus(resourcePack)
-
-        defaultResourcePacksField.set(Minecraft.getMinecraft(), packs)
 
         return manifest
     }
