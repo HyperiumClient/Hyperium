1d4b235c4cf386e1229c7037f4cc9fc605c4f897
Simcha van Collem <simcha@vancollem.nl>
2018-04-20 13:43:04 +0200

    radio button is now diabled when addon url doesnt exist. prevents people trying to download not released addons

--- src/main/java/cc/hyperium/installer/components/MaterialRadioButton.java
+++ src/main/java/cc/hyperium/installer/components/MaterialRadioButton.java
@@ -1,37 +1,48 @@
 package cc.hyperium.installer.components;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
+
+import javax.swing.*;
 
 /*
  * Created by Cubxity on 19/04/2018
  */
 public class MaterialRadioButton extends JRadioButton {
-    private static BufferedImage b;
-    private static BufferedImage sb;
-    static{
-         b = new BufferedImage(
-                10, 10, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D bg = b.createGraphics();
-        bg.setPaint(Color.WHITE);
-        bg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        bg.draw(new Ellipse2D.Double(0, 0, 9, 9));
-        sb = new BufferedImage(
-                10, 10, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D sbg = sb.createGraphics();
-        sbg.setPaint(Color.WHITE);
-        sbg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        sbg.draw(new Ellipse2D.Double(0, 0, 9, 9));
-        sbg.fill(new Ellipse2D.Double(2, 2, 6, 6));
-    }
-    public MaterialRadioButton(String label) {
-        super(label);
-        setForeground(Color.WHITE);
-        setBackground(new Color(30, 30, 30));
-        setIcon(new ImageIcon(b));
-        setSelectedIcon(new ImageIcon(sb));
-        setFocusPainted(false);
-    }
+
+	private static BufferedImage unselectedButtonImage;
+    private static BufferedImage selectedButtonImage;
+    private static BufferedImage disabledButtonImage;
+
+	static {
+		unselectedButtonImage = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = unselectedButtonImage.createGraphics();
+		graphics.setPaint(Color.WHITE);
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics.draw(new Ellipse2D.Double(0, 0, 9, 9));
+
+		selectedButtonImage = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+		graphics = selectedButtonImage.createGraphics();
+		graphics.setPaint(Color.WHITE);
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics.draw(new Ellipse2D.Double(0, 0, 9, 9));
+		graphics.fill(new Ellipse2D.Double(2, 2, 6, 6));
+
+        disabledButtonImage = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+        graphics = disabledButtonImage.createGraphics();
+        graphics.setPaint(Color.GRAY);
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics.draw(new Ellipse2D.Double(0, 0, 9, 9));
+	}
+
+	public MaterialRadioButton(String label) {
+		super(label);
+		setForeground(Color.WHITE);
+		setBackground(new Color(30, 30, 30));
+		setIcon(new ImageIcon(unselectedButtonImage));
+		setSelectedIcon(new ImageIcon(selectedButtonImage));
+		setDisabledIcon(new ImageIcon(disabledButtonImage));
+		setFocusPainted(false);
+	}
 }
