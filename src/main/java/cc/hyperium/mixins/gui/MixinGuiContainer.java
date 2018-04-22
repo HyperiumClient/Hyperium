b20823a6bc13370681c87f20ea30f72890b5652e
bugfroggy <bugfroggy@gmail.com>
2018-04-21 20:41:58 -0400

    added basic GUI background blurring. TODO togglable

---
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiContainer.java
@@ -0,0 +1,22 @@
+package cc.hyperium.mixins.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiContainer.class)
+public abstract class MixinGuiContainer extends GuiScreen {
+
+
+
+    /**
+     * @see MixinGuiScreen#onGuiClosed(CallbackInfo)
+     */
+    @Inject(method = "onGuiClosed", at = @At("HEAD"))
+    private void onGuiClosed(CallbackInfo ci) {
+        super.onGuiClosed();
+    }
+}
--- src/main/java/cc/hyperium/mixins/gui/MixinGuiScreen.java
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiScreen.java
@@ -20,11 +20,16 @@ package cc.hyperium.mixins.gui;
 import cc.hyperium.gui.settings.items.BackgroundSettings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @Mixin(GuiScreen.class)
 public abstract class MixinGuiScreen {
@@ -38,4 +43,35 @@ public abstract class MixinGuiScreen {
         }
     }
 
+    @Inject(method = "initGui", at = @At("HEAD"))
+    private void initGui(CallbackInfo ci) {
+        Minecraft.getMinecraft().addScheduledTask(() -> {
+            Method loadShaderMethod = null;
+            try {
+                loadShaderMethod = EntityRenderer.class.getDeclaredMethod("loadShader", ResourceLocation.class);
+            } catch (NoSuchMethodException e) {
+                try {
+                    loadShaderMethod = EntityRenderer.class.getDeclaredMethod("a", ResourceLocation.class);
+                } catch (NoSuchMethodException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            if (loadShaderMethod != null) {
+                loadShaderMethod.setAccessible(true);
+                try {
+                    loadShaderMethod.invoke(Minecraft.getMinecraft().entityRenderer, new ResourceLocation("shaders/hyperium_blur.json"));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Inject(method = "onGuiClosed", at = @At("HEAD"))
+    private void onGuiClosed(CallbackInfo ci) {
+        Minecraft.getMinecraft().addScheduledTask(() -> {
+            Minecraft.getMinecraft().entityRenderer.stopUseShader();
+        });
+    }
 }
