4f5a10d3b7f9ba4ef92e0d5ed5f93704de554bea
Simcha van Collem <simcha@vancollem.nl>
2018-04-30 16:37:28 +0200

    Knee and elbow support for cosmetics (#179)
    * Added knee and elbow support
    
    * added Pre- and PostCopyPlayerModelAnglesEvent
    
    * added the entity to the event
    
    * offset wasnt copied before. resulted in strange things
    
    * removed since the event was never used and the mixin was causing issues with other mixins
    
    * added MixinModelBiped.java and IMixinModelBiped.java for rendering armor
    
    * - added a fix for textures displaying at wrong parts.
    - IMixinModelPlayer now extends IMixinModelBiped so we can call them in one event
    
    * moved IMixinModelPlayer to another package in another commit
    
    * updated event to work  for ModelBiped and ModelPlayer. Dab and floss code is edited so it works with the new event. MixinDabRender is removed since the dabbing and flossing is now done in an event
    
    * - added a fix for boots not displaying when they were equipped
    - merged two MixinRenderPlayer classes into one
    - updated everything in the mixins.hyperium.json

--- src/main/java/cc/hyperium/handlers/handlers/DabHandler.java
+++ src/main/java/cc/hyperium/handlers/handlers/DabHandler.java
@@ -1,14 +1,15 @@
 package cc.hyperium.handlers.handlers;
 
 import cc.hyperium.event.InvokeEvent;
+import cc.hyperium.event.PreCopyPlayerModelAnglesEvent;
 import cc.hyperium.event.RenderEvent;
 import cc.hyperium.event.WorldChangeEvent;
 import cc.hyperium.gui.HyperiumGui;
 import cc.hyperium.gui.settings.items.CosmeticSettings;
+import cc.hyperium.mixinsimp.renderer.model.IMixinModelBiped;
+import cc.hyperium.mixinsimp.renderer.model.IMixinModelPlayer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.model.ModelPlayer;
 
 import java.util.Map;
 import java.util.UUID;
@@ -60,6 +61,18 @@ public class DabHandler {
         }
     }
 
+    @InvokeEvent
+    public void onPreCopyPlayerModelAngles(PreCopyPlayerModelAnglesEvent event) {
+        AbstractClientPlayer entity = event.getEntity();
+        IMixinModelBiped player = event.getModel();
+
+        if (player instanceof IMixinModelPlayer) {
+            modify(entity, ((IMixinModelPlayer) player));
+        } else {
+            modify(entity, player);
+        }
+    }
+
     public int getDabs() {
         return dabs;
     }
@@ -81,98 +94,98 @@ public class DabHandler {
         get(uuid).stopDabbing();
     }
 
-    public void modify(AbstractClientPlayer entity, ModelPlayer player) {
+    public void modify(AbstractClientPlayer entity, IMixinModelPlayer player) {
         int ticks = get(entity.getUniqueID()).dabFrames;
 
         if (ticks <= 0) return;
 
         float heldPercent = state / 100F;
         if (right) {
-            player.bipedRightArm.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
-            player.bipedRightArm.rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
 
-            player.bipedRightArmwear.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
-            player.bipedRightArmwear.rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
+            player.getBipedRightUpperArmwear().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
+            player.getBipedRightUpperArmwear().rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
 
-            player.bipedLeftArm.rotateAngleX = (float) Math.toRadians(15.0f * heldPercent);
-            player.bipedLeftArm.rotateAngleY = (float) Math.toRadians(15.0f * heldPercent);
-            player.bipedLeftArm.rotateAngleZ = (float) Math.toRadians(-110.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleX = (float) Math.toRadians(15.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleY = (float) Math.toRadians(15.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleZ = (float) Math.toRadians(-110.0f * heldPercent);
 
-            player.bipedLeftArmwear.rotateAngleX = (float) Math.toRadians(15.0f * heldPercent);
-            player.bipedLeftArmwear.rotateAngleY = (float) Math.toRadians(15.0f * heldPercent);
-            player.bipedLeftArmwear.rotateAngleZ = (float) Math.toRadians(-110.0f * heldPercent);
+            player.getBipedLeftUpperArmwear().rotateAngleX = (float) Math.toRadians(15.0f * heldPercent);
+            player.getBipedLeftUpperArmwear().rotateAngleY = (float) Math.toRadians(15.0f * heldPercent);
+            player.getBipedLeftUpperArmwear().rotateAngleZ = (float) Math.toRadians(-110.0f * heldPercent);
 
             final float rotationX = entity.rotationPitch;
             final float rotationY = entity.renderYawOffset - entity.rotationYaw;
 
-            player.bipedHead.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
-            player.bipedHead.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
+            player.getBipedHead().rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
+            player.getBipedHead().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
 
-            player.bipedHeadwear.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
-            player.bipedHeadwear.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
+            player.getBipedHeadwear().rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
+            player.getBipedHeadwear().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
 
         } else {
-            player.bipedLeftArm.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
-            player.bipedLeftArm.rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
 
-            player.bipedLeftArmwear.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
-            player.bipedLeftArmwear.rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
+            player.getBipedLeftUpperArmwear().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
+            player.getBipedLeftUpperArmwear().rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
 
-            player.bipedRightArm.rotateAngleX = (float) Math.toRadians(-15.0f * heldPercent);
-            player.bipedRightArm.rotateAngleY = (float) Math.toRadians(-15.0f * heldPercent);
-            player.bipedRightArm.rotateAngleZ = (float) Math.toRadians(110.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleX = (float) Math.toRadians(-15.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleY = (float) Math.toRadians(-15.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleZ = (float) Math.toRadians(110.0f * heldPercent);
 
-            player.bipedRightArmwear.rotateAngleX = (float) Math.toRadians(-15.0f * heldPercent);
-            player.bipedRightArmwear.rotateAngleY = (float) Math.toRadians(-15.0f * heldPercent);
-            player.bipedRightArmwear.rotateAngleZ = (float) Math.toRadians(110.0f * heldPercent);
+            player.getBipedRightUpperArmwear().rotateAngleX = (float) Math.toRadians(-15.0f * heldPercent);
+            player.getBipedRightUpperArmwear().rotateAngleY = (float) Math.toRadians(-15.0f * heldPercent);
+            player.getBipedRightUpperArmwear().rotateAngleZ = (float) Math.toRadians(110.0f * heldPercent);
 
 
             final float rotationX = entity.rotationPitch;
-            player.bipedHead.rotateAngleX =
+            player.getBipedHead().rotateAngleX =
                     (float) Math.toRadians(-rotationX * heldPercent) +
                             (float) Math.toRadians(45.0f * heldPercent + rotationX);
             final float rotationY = entity.renderYawOffset - entity.rotationYaw;
 
-            player.bipedHead.rotateAngleY =
+            player.getBipedHead().rotateAngleY =
                     (float) Math.toRadians(rotationY * heldPercent) +
                             (float) Math.toRadians(-35.0f * heldPercent - rotationY);
 
-            player.bipedHeadwear.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
-            player.bipedHeadwear.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
+            player.getBipedHeadwear().rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
+            player.getBipedHeadwear().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
 
         }
     }
 
-    public void modify(AbstractClientPlayer entity, ModelBiped player) {
+    public void modify(AbstractClientPlayer entity, IMixinModelBiped player) {
         int ticks = get(entity.getUniqueID()).dabFrames;
 
         if (ticks <= 0) return;
 
         float heldPercent = state / 100F;
         if (right) {
-            player.bipedRightArm.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
-            player.bipedRightArm.rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
-            player.bipedLeftArm.rotateAngleX = (float) Math.toRadians(15.0f * heldPercent);
-            player.bipedLeftArm.rotateAngleY = (float) Math.toRadians(15.0f * heldPercent);
-            player.bipedLeftArm.rotateAngleZ = (float) Math.toRadians(-110.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleX = (float) Math.toRadians(15.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleY = (float) Math.toRadians(15.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleZ = (float) Math.toRadians(-110.0f * heldPercent);
             final float rotationX = entity.rotationPitch;
-            player.bipedHead.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
+            player.getBipedHead().rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
             final float rotationY = entity.renderYawOffset - entity.rotationYaw;
-            player.bipedHead.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
-            player.bipedHeadwear.rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
-            player.bipedHeadwear.rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
+            player.getBipedHead().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(35.0f * heldPercent - rotationY);
+            player.getBipedHeadwear().rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
+            player.getBipedHeadwear().rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
         } else {
-            player.bipedLeftArm.rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
-            player.bipedLeftArm.rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
-            player.bipedRightArm.rotateAngleX = (float) Math.toRadians(-15.0f * heldPercent);
-            player.bipedRightArm.rotateAngleY = (float) Math.toRadians(-15.0f * heldPercent);
-            player.bipedRightArm.rotateAngleZ = (float) Math.toRadians(110.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
+            player.getBipedLeftUpperArm().rotateAngleY = (float) Math.toRadians(35.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleX = (float) Math.toRadians(-15.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleY = (float) Math.toRadians(-15.0f * heldPercent);
+            player.getBipedRightUpperArm().rotateAngleZ = (float) Math.toRadians(110.0f * heldPercent);
             final float rotationX = entity.rotationPitch;
-            player.bipedHead.rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
+            player.getBipedHead().rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
             final float rotationY = entity.renderYawOffset - entity.rotationYaw;
-            player.bipedHead.rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
-            player.bipedHeadwear.rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
-            player.bipedHeadwear.rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
+            player.getBipedHead().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
+            player.getBipedHeadwear().rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
+            player.getBipedHeadwear().rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
         }
     }
 
---
+++ src/main/java/cc/hyperium/mixins/renderer/model/MixinModelBox.java
@@ -0,0 +1,30 @@
+package cc.hyperium.mixins.renderer.model;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import cc.hyperium.mixinsimp.renderer.model.IMixinModelBox;
+import net.minecraft.client.model.ModelBox;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.model.TexturedQuad;
+
+@Mixin(ModelBox.class)
+public class MixinModelBox implements IMixinModelBox {
+
+	@Shadow
+	private TexturedQuad[] quadList;
+
+	@Override
+	public void offsetTextureQuad(ModelRenderer renderer, int quadId, float xOffset, float yOffset) {
+		if (quadId >= 0 & quadId < this.quadList.length) {
+			this.quadList[quadId].vertexPositions[0].texturePositionX += xOffset / renderer.textureWidth;
+			this.quadList[quadId].vertexPositions[1].texturePositionX += xOffset / renderer.textureWidth;
+			this.quadList[quadId].vertexPositions[2].texturePositionX += xOffset / renderer.textureWidth;
+			this.quadList[quadId].vertexPositions[3].texturePositionX += xOffset / renderer.textureWidth;
+			this.quadList[quadId].vertexPositions[0].texturePositionY += yOffset / renderer.textureHeight;
+			this.quadList[quadId].vertexPositions[1].texturePositionY += yOffset / renderer.textureHeight;
+			this.quadList[quadId].vertexPositions[2].texturePositionY += yOffset / renderer.textureHeight;
+			this.quadList[quadId].vertexPositions[3].texturePositionY += yOffset / renderer.textureHeight;
+		}
+	}
+}
