1d7f1d6dbedd858cacf8afb832c7bb855d7e48b6
boomboompower <boomboompowermc@gmail.com>
2018-04-30 21:02:33 +1000

    Begin working on custom backgrounds on more menus

---
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiOptions.java
@@ -0,0 +1,25 @@
+package cc.hyperium.mixins.gui;
+
+import net.minecraft.client.gui.GuiOptions;
+import net.minecraft.client.gui.GuiScreen;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiOptions.class)
+public class MixinGuiOptions extends GuiScreen {
+    
+    /**
+     * @reason Snap done to bottom of the screen
+     */
+    @Inject(method = "initGui", at = @At(value = "RETURN"))
+    public void initGui(CallbackInfo c) {
+        this.buttonList.forEach(b -> {
+            if (b.id == 200) {
+                b.yPosition = this.height - 30;
+            }
+        });
+    }
+}
