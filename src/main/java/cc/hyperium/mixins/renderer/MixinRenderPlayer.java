ba4369f7c51e98ead14cf3ec7135d901518ba91d
Simcha van Collem <simcha@vancollem.nl>
2018-04-30 15:43:06 +0200

    - added a fix for boots not displaying when they were equipped - merged two MixinRenderPlayer classes into one - updated everything in the mixins.hyperium.json

--- src/main/java/cc/hyperium/mixins/renderer/MixinRenderPlayer.java
+++ src/main/java/cc/hyperium/mixins/renderer/MixinRenderPlayer.java
@@ -17,37 +17,64 @@
 
 package cc.hyperium.mixins.renderer;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.RenderPlayerEvent;
+import cc.hyperium.mixinsimp.renderer.layers.TwoPartLayerBipedArmor;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.EntityLivingBase;
 
 @Mixin(RenderPlayer.class)
 public abstract class MixinRenderPlayer extends RendererLivingEntity<AbstractClientPlayer> {
+
+    @Shadow
+    public abstract ModelPlayer getMainModel();
 
     public MixinRenderPlayer(RenderManager renderManagerIn, ModelBase modelBaseIn, float shadowSizeIn) {
         super(renderManagerIn, modelBaseIn, shadowSizeIn);
     }
 
-    @Shadow
-    public abstract ModelPlayer getMainModel();
+	/**
+	 * Not using the normal armor layer, but a slightly modified one. This is done
+	 * to prevent weird rendering bugs because of armor being on different layers in
+	 * the textures. These bugs were caused because the armor needs to be slit up in
+	 * two for the knees and elbows
+	 *
+	 * @author 9Y0
+	 */
+    @SuppressWarnings("unchecked")
+    @ModifyArg(method = "<init>(Lnet/minecraft/client/renderer/entity/RenderManager;Z)V", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/renderer/entity/RenderPlayer;addLayer(Lnet/minecraft/client/renderer/entity/layers/LayerRenderer;)Z"))
+    private <V extends EntityLivingBase, U extends LayerRenderer<V>> U injectTwoPartLayerBipedArmor(U original) {
+        return (U) new TwoPartLayerBipedArmor(this);
+    }
+
     @Inject(method = "doRender", at = @At("HEAD"))
     private void doRender(AbstractClientPlayer entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo ci) {
         EventBus.INSTANCE.post(new RenderPlayerEvent(entity, renderManager, x, y, z, partialTicks));
-
-        //Start DAB
-
     }
 
-
+    /**
+     * Fixes bug MC-1349
+     *
+     * @param clientPlayer
+     * @param ci
+     */
+    @Inject(method = "renderRightArm", at = @At(value = "FIELD", ordinal = 3))
+    private void onUpdateTimer(AbstractClientPlayer clientPlayer, CallbackInfo ci) {
+        ModelPlayer modelplayer = this.getMainModel();
+        modelplayer.isRiding = modelplayer.isSneak = false;
+    }
 }
 
