9f7c632a22e20a2a926b7dc2265f05de573a4f79
VRCube <vrcproxz@gmail.com>
2018-05-01 09:16:56 +0200

    retry if no work

--- src/main/java/cc/hyperium/gui/GuiHyperiumCredits.java
+++ src/main/java/cc/hyperium/gui/GuiHyperiumCredits.java
@@ -30,39 +30,45 @@ public class GuiHyperiumCredits extends HyperiumGui {
 
     private static LinkedHashMap<String, DynamicTexture> contributors = new LinkedHashMap<>();
     private static HashMap<String, Integer> commits = new HashMap<>();
+    private static String err = "";
     private GuiScreen prevGui;
     private int offY = 0;
 
     static {
-        JsonParser parser = new JsonParser();
         System.setProperty("http.agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
-        Multithreading.runAsync(() -> {
-            try {
-                String content = IOUtils.toString(URI.create("https://api.github.com/repos/HyperiumClient/Hyperium/stats/contributors"));
-                JsonArray a = parser.parse(content).getAsJsonArray();
-                StreamSupport.stream(a.spliterator(), false).map(JsonElement::getAsJsonObject).filter(o -> o.has("total") & o.get("total").getAsInt() > 20).sorted(Comparator.comparingLong(o -> ((JsonObject) o).get("total").getAsInt()).reversed()).forEach(o -> {
-                    JsonObject con = o.get("author").getAsJsonObject();
-                    try {
-                        BufferedImage bi = ImageIO.read(new URL(con.get("avatar_url").getAsString() + "?size=20"));
-                        Minecraft.getMinecraft().addScheduledTask(()-> {
-                            DynamicTexture tx = new DynamicTexture(bi);
-                            try {
-                                tx.loadTexture(Minecraft.getMinecraft().getResourceManager());
-                                contributors.put(con.get("login").getAsString(), tx);
-                                commits.put(con.get("login").getAsString(), o.get("total").getAsInt());
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }).get();
-                    } catch (IOException | InterruptedException | ExecutionException e) {
-                        e.printStackTrace();
-                    }
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        Multithreading.runAsync(() -> fetch(1));
+    }
 
+    private static void fetch(int tries) {
+        try {
+            String content = IOUtils.toString(URI.create("https://api.github.com/repos/HyperiumClient/Hyperium/stats/contributors"));
+            JsonParser parser = new JsonParser();
+            JsonArray a = parser.parse(content).getAsJsonArray();
+            StreamSupport.stream(a.spliterator(), false).map(JsonElement::getAsJsonObject).filter(o -> o.has("total") & o.get("total").getAsInt() > 20).sorted(Comparator.comparingLong(o -> ((JsonObject) o).get("total").getAsInt()).reversed()).forEach(o -> {
+                JsonObject con = o.get("author").getAsJsonObject();
+                try {
+                    BufferedImage bi = ImageIO.read(new URL(con.get("avatar_url").getAsString() + "?size=20"));
+                    Minecraft.getMinecraft().addScheduledTask(() -> {
+                        DynamicTexture tx = new DynamicTexture(bi);
+                        try {
+                            tx.loadTexture(Minecraft.getMinecraft().getResourceManager());
+                            contributors.put(con.get("login").getAsString(), tx);
+                            commits.put(con.get("login").getAsString(), o.get("total").getAsInt());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).get();
+                } catch (IOException | InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tries < 5)
+                fetch(tries + 1);
+            else
+                err = e.getMessage();
+        }
     }
 
     public GuiHyperiumCredits(GuiScreen prevGui) {
@@ -73,19 +79,18 @@ public class GuiHyperiumCredits extends HyperiumGui {
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         FontRenderer fr = Minecraft.getMinecraft().fontRendererObj;
         GlStateManager.scale(2f, 2f, 2f);
-        drawCenteredString(fr, contributors.isEmpty() ? "Loading contributors..." : "Contributors", width / 4, 20 + offY / 2, 0xFFFFFF);
+        drawCenteredString(fr, err.isEmpty() ? (contributors.isEmpty() ? "Loading contributors..." : "Contributors") : "Error: "+err, width / 4, 20 + offY / 2, 0xFFFFFF);
         GlStateManager.scale(0.5f, 0.5f, 0.5f);
         int x = width / 2 - 100;
         AtomicInteger y = new AtomicInteger(70 + offY);
         contributors.forEach((c, i) -> {
             GlStateManager.color(1f, 1f, 1f);
             GlStateManager.bindTexture(i.getGlTextureId());
-            drawScaledCustomSizeModalRect(x, y.get(), 0, 0, 20,     20, 20, 20, 20, 20);
+            drawScaledCustomSizeModalRect(x, y.get(), 0, 0, 20, 20, 20, 20, 20, 20);
             drawString(fr, c, x + 25, y.get(), 0xE0E0E0);
             drawString(fr, commits.get(c) + " commits", x + 25, y.get() + 10, 0x757575);
             y.addAndGet(25);
         });
-
     }
 
     @Override
@@ -97,7 +102,7 @@ public class GuiHyperiumCredits extends HyperiumGui {
     public void handleMouseInput() throws IOException {
         super.handleMouseInput();
         int i = Mouse.getEventDWheel();
-        if(i < 0)
+        if (i < 0)
             offY -= 10;
         else if (i > 0)
             offY += 10;
