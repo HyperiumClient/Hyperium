8f3279f9d37749280b3a1e267e6ed3ebf4973df9
Simcha van Collem <simcha@vancollem.nl>
2018-05-02 13:53:02 +0200

    refactored DabHandler and removed all the duplicate code

--- src/main/java/cc/hyperium/handlers/handlers/animation/DabHandler.java
+++ src/main/java/cc/hyperium/handlers/handlers/animation/DabHandler.java
@@ -15,62 +15,28 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class DabHandler {
+public class DabHandler extends AbstractAnimationHandler<PreCopyPlayerModelAnglesEvent> {
 
-    private final Map<UUID, DabState> dabStates = new ConcurrentHashMap<>();
     private int dabs;
-    private float state = 0;
-    private boolean right = true;
-    private boolean asc = true;
-    private long systemTime = 0;
-
-    @InvokeEvent
-    public void swapWorld(WorldChangeEvent event) {
-        dabStates.clear();
-    }
 
     @InvokeEvent
     public void onRender(RenderEvent e) {
-        dabStates.values().forEach(DabState::update);
-
-        if (state == 100 && get(Minecraft.getMinecraft().thePlayer.getUniqueID()).isDabbing())
+        if (state == 100 && get(Minecraft.getMinecraft().thePlayer.getUniqueID()).isAnimating())
             incDabs();
-
-        if (this.systemTime == 0) this.systemTime = Minecraft.getSystemTime();
-
-        if (this.systemTime < Minecraft.getSystemTime() + (1000 / 120)) {
-            state = HyperiumGui.clamp(
-                    HyperiumGui.easeOut(
-                            this.state,
-                            this.asc ? 100.0f : 0.0f,
-                            0.01f,
-                            CosmeticSettings.dabSpeed
-                    ),
-                    0.0f,
-                    100.0f
-            );
-
-            this.systemTime += (1000 / 120);
-
-            if (state <= 0) {
-                asc = true;
-                right = !right;
-            } else if (state >= 100) {
-                asc = false;
-            }
-        }
     }
 
-    @InvokeEvent
-    public void onPreCopyPlayerModelAngles(PreCopyPlayerModelAnglesEvent event) {
-        AbstractClientPlayer entity = event.getEntity();
-        IMixinModelBiped player = event.getModel();
-
-        if (player instanceof IMixinModelPlayer) {
-            modify(entity, ((IMixinModelPlayer) player));
-        } else {
-            modify(entity, player);
-        }
+    @Override
+    public float modifyState(float currentState) {
+        return HyperiumGui.clamp(
+                HyperiumGui.easeOut(
+                        this.state,
+                        this.asc ? 100.0f : 0.0f,
+                        0.01f,
+                        CosmeticSettings.dabSpeed
+                ),
+                0.0f,
+                100.0f
+        );
     }
 
     public int getDabs() {
@@ -81,25 +47,8 @@ public class DabHandler {
         dabs++;
     }
 
-    public DabState get(UUID uuid) {
-        return dabStates.computeIfAbsent(uuid, r -> new DabState());
-    }
-
-    public void startDabbing(UUID uuid) {
-        get(uuid).ensureDabbingFor(60);
-    }
-
-    public void stopDabbing(UUID uuid) {
-        get(uuid).setToggled(false);
-        get(uuid).stopDabbing();
-    }
-
-    public void modify(AbstractClientPlayer entity, IMixinModelPlayer player) {
-        int ticks = get(entity.getUniqueID()).dabFrames;
-
-        if (ticks <= 0) return;
-
-        float heldPercent = state / 100F;
+    @Override
+    public void modifyPlayer(AbstractClientPlayer entity, IMixinModelPlayer player, float heldPercent) {
         if (right) {
             player.getBipedRightUpperArm().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
             player.getBipedRightUpperArm().rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
@@ -141,14 +90,10 @@ public class DabHandler {
 
 
             final float rotationX = entity.rotationPitch;
-            player.getBipedHead().rotateAngleX =
-                    (float) Math.toRadians(-rotationX * heldPercent) +
-                            (float) Math.toRadians(45.0f * heldPercent + rotationX);
+            player.getBipedHead().rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
             final float rotationY = entity.renderYawOffset - entity.rotationYaw;
 
-            player.getBipedHead().rotateAngleY =
-                    (float) Math.toRadians(rotationY * heldPercent) +
-                            (float) Math.toRadians(-35.0f * heldPercent - rotationY);
+            player.getBipedHead().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
 
             player.getBipedHeadwear().rotateAngleX = (float) Math.toRadians(-rotationX * heldPercent) + (float) Math.toRadians(45.0f * heldPercent + rotationX);
             player.getBipedHeadwear().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
@@ -156,12 +101,8 @@ public class DabHandler {
         }
     }
 
-    public void modify(AbstractClientPlayer entity, IMixinModelBiped player) {
-        int ticks = get(entity.getUniqueID()).dabFrames;
-
-        if (ticks <= 0) return;
-
-        float heldPercent = state / 100F;
+    @Override
+    public void modifyPlayer(AbstractClientPlayer entity, IMixinModelBiped player, float heldPercent) {
         if (right) {
             player.getBipedRightUpperArm().rotateAngleX = (float) Math.toRadians(-90.0f * heldPercent);
             player.getBipedRightUpperArm().rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
@@ -186,50 +127,6 @@ public class DabHandler {
             player.getBipedHead().rotateAngleY = (float) Math.toRadians(rotationY * heldPercent) + (float) Math.toRadians(-35.0f * heldPercent - rotationY);
             player.getBipedHeadwear().rotateAngleX = (float) Math.toRadians(45.0f * heldPercent);
             player.getBipedHeadwear().rotateAngleY = (float) Math.toRadians(-35.0f * heldPercent);
-        }
-    }
-
-    public class DabState {
-        int dabFrames = 0;
-        long systemTime;
-        boolean toggled;
-
-        public DabState() {
-            this.systemTime = Minecraft.getSystemTime();
-            this.toggled = false;
-        }
-
-        void update() {
-            while (this.systemTime < Minecraft.getSystemTime() + (1000 / 60)) {
-                this.dabFrames--;
-                this.systemTime += (1000 / 60);
-            }
-
-            if (dabFrames <= 0) {
-                if (CosmeticSettings.dabToggle && this.toggled) {
-                    ensureDabbingFor(60);
-                } else {
-                    dabFrames = 0;
-                }
-            }
-        }
-
-        public void ensureDabbingFor(int seconds) {
-            seconds *= 60;
-
-            dabFrames = Math.max(dabFrames, seconds);
-        }
-
-        public void stopDabbing() {
-            dabFrames = 0;
-        }
-
-        public boolean isDabbing() {
-            return dabFrames > 0;
-        }
-
-        public void setToggled(boolean toggled) {
-            this.toggled = toggled;
         }
     }
 }
