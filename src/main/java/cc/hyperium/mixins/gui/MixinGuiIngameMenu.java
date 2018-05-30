38d62a3fdf6c75d7ba4b1f8d408761d8a1fba2ff
KodingKing <KodingKing1@users.noreply.github.com>
2018-05-30 22:14:56 +1000

    Added ingame server changer (#232)
    Added server changer ingame.

--- src/main/java/cc/hyperium/mixins/gui/MixinGuiIngameMenu.java
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiIngameMenu.java
@@ -1,10 +1,10 @@
 package cc.hyperium.mixins.gui;
 
 import cc.hyperium.gui.GuiHyperiumCredits;
+import cc.hyperium.gui.GuiIngameMultiplayer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiIngameMenu;
-import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.*;
+import net.minecraft.world.WorldServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,12 +16,21 @@ public class MixinGuiIngameMenu extends GuiScreen {
     @Inject(method = "initGui", at = @At("RETURN"))
     public void initGui(CallbackInfo ci) {
         this.buttonList.add(new GuiButton(8, this.width - 200, this.height - 20, 200, 20, "Credits"));
+//        this.buttonList.get(3).displayString = Minecraft.getMinecraft().getIntegratedServer().getPublic() ? "Change Server" : this.buttonList.get(3).displayString;
+//        this.buttonList.get(3).enabled = Minecraft.getMinecraft().getIntegratedServer().getPublic() ? true : this.buttonList.get(3).enabled;
+        if (Minecraft.getMinecraft().theWorld.isRemote) {
+            GuiButton oldButton = buttonList.remove(3);
+            GuiButton newButton = new GuiButton(10, oldButton.xPosition, oldButton.yPosition, oldButton.getButtonWidth(), 20, "Server List");
+            buttonList.add(newButton);
+        }
     }
 
     @Inject(method = "actionPerformed", at = @At("HEAD"))
     public void actionPerformed(GuiButton button, CallbackInfo ci) {
         if (button.id == 8)
             Minecraft.getMinecraft().displayGuiScreen(new GuiHyperiumCredits(Minecraft.getMinecraft().currentScreen));
+        if (button.id == 10 && Minecraft.getMinecraft().theWorld.isRemote)
+            Minecraft.getMinecraft().displayGuiScreen(new GuiIngameMultiplayer(Minecraft.getMinecraft().currentScreen));
     }
 
 }
