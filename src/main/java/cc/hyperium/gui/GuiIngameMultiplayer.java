31c81692030fdf3c95f77b7ef859f911742e0789
Dylan <djitechdude@gmail.com>
2018-05-30 20:08:43 +1000

    Added server changer ingame.

---
+++ src/main/java/cc/hyperium/gui/GuiIngameMultiplayer.java
@@ -0,0 +1,44 @@
+package cc.hyperium.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiMultiplayer;
+import net.minecraft.client.gui.GuiScreen;
+
+import java.io.IOException;
+
+public class GuiIngameMultiplayer extends GuiMultiplayer {
+
+    public GuiIngameMultiplayer(GuiScreen parentScreen) {
+        super(parentScreen);
+    }
+
+    /*
+           Ported by KodingKing
+           Original mod by Canelex
+    */
+
+    public void actionPerformed(GuiButton button) throws IOException {
+        if (button.id == 1 || button.id == 4) {
+            dc();
+        }
+        super.actionPerformed(button);
+    }
+
+    public void connectToSelected() {
+        dc();
+        super.connectToSelected();
+    }
+
+    private void dc() {
+        if (Minecraft.getMinecraft().theWorld != null) {
+            Minecraft.getMinecraft().theWorld.sendQuittingDisconnectingPacket();
+            Minecraft.getMinecraft().loadWorld(null);
+            Minecraft.getMinecraft().displayGuiScreen(null);
+        }
+    }
+
+    /*
+        End port
+     */
+}
--- src/main/java/cc/hyperium/mixins/gui/MixinGuiMultiplayer.java
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiMultiplayer.java
@@ -1,18 +1,29 @@
 package cc.hyperium.mixins.gui;
 
 import cc.hyperium.mixinsimp.renderer.gui.IMixinGuiMultiplayer;
-import net.minecraft.client.gui.GuiMultiplayer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.*;
 import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.network.play.server.S40PacketDisconnect;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiMultiplayer.class)
-public class MixinGuiMultiplayer implements IMixinGuiMultiplayer {
+public abstract class MixinGuiMultiplayer implements IMixinGuiMultiplayer {
     @Shadow
     private boolean directConnect;
     @Shadow
     private ServerData selectedServer;
 
+    @Shadow private GuiScreen parentScreen;
+
+    @Shadow protected abstract void connectToServer(ServerData server);
+
+    @Shadow private ServerSelectionList serverListSelector;
     @Override
     public void makeDirectConnect() {
         directConnect = true;
@@ -22,4 +33,5 @@ public class MixinGuiMultiplayer implements IMixinGuiMultiplayer {
     public void setIp(ServerData ip) {
         this.selectedServer = ip;
     }
+
 }
