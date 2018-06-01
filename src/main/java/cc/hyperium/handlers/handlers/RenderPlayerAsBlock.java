0a4eadbcda9bb23fb505ae75eb607360e2d27cc2
Mitchell Katz <sk1er@lythrim.net>
2018-06-01 16:01:56 -0400

    Update but still not quite working

--- src/main/java/cc/hyperium/handlers/handlers/RenderPlayerAsBlock.java
+++ src/main/java/cc/hyperium/handlers/handlers/RenderPlayerAsBlock.java
@@ -1,17 +1,18 @@
 package cc.hyperium.handlers.handlers;
 
 import cc.hyperium.mixinsimp.renderer.IMixinRenderManager;
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.RegionRenderCache;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormatElement;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class RenderPlayerAsBlock {
 
@@ -20,50 +21,19 @@ public class RenderPlayerAsBlock {
         if (entityWorld != null) {
             IMixinRenderManager renderManager = (IMixinRenderManager) Minecraft.getMinecraft().getRenderManager();
             int x1 = (int) (x + renderManager.getPosX());
-            int y1 = (int) (y + renderManager.getPosY()-1);
+            int y1 = (int) (y + renderManager.getPosY() - 1);
             int z1 = (int) (z + renderManager.getPosZ());
-            System.out.println("x1 = " + x1);
-            System.out.println("y1 = " + y1);
-            System.out.println("z1 = " + z1);
-            IBlockState blockState = entityWorld.getBlockState(new BlockPos(x1, y1, z1));
-            String res1 = "air";
-            if (blockState != null) {
-                Block block = blockState.getBlock();
-                System.out.println(block.getMaterial());
-                res1 = block.getUnlocalizedName().replaceFirst("tile.", "");
-            }
-            System.out.println(res1);
-            final ResourceLocation res = new ResourceLocation("textures/blocks/" + res1 + ".png");
-            Minecraft.getMinecraft().getTextureManager().bindTexture(res);
-            GlStateManager.color(0.5f, 0.5f, 0.5f);
-            GlStateManager.disableLighting();
-            final WorldRenderer wr = Tessellator.getInstance().getWorldRenderer();
-            wr.begin(7, DefaultVertexFormats.POSITION_TEX);
-            wr.pos(-0.5, 1.0, 0.5).tex(1.0, 0.0).endVertex();
-            wr.pos(-0.5, 0.0, 0.5).tex(1.0, 1.0).endVertex();
-            wr.pos(0.5, 0.0, 0.5).tex(0.0, 1.0).endVertex();
-            wr.pos(0.5, 1.0, 0.5).tex(0.0, 0.0).endVertex();
-            wr.pos(0.5, 1.0, 0.5).tex(1.0, 0.0).endVertex();
-            wr.pos(0.5, 0.0, 0.5).tex(1.0, 1.0).endVertex();
-            wr.pos(0.5, 0.0, -0.5).tex(0.0, 1.0).endVertex();
-            wr.pos(0.5, 1.0, -0.5).tex(0.0, 0.0).endVertex();
-            wr.pos(0.5, 1.0, -0.5).tex(1.0, 0.0).endVertex();
-            wr.pos(0.5, 0.0, -0.5).tex(1.0, 1.0).endVertex();
-            wr.pos(-0.5, 0.0, -0.5).tex(0.0, 1.0).endVertex();
-            wr.pos(-0.5, 1.0, -0.5).tex(0.0, 0.0).endVertex();
-            wr.pos(-0.5, 1.0, -0.5).tex(1.0, 0.0).endVertex();
-            wr.pos(-0.5, 0.0, -0.5).tex(1.0, 1.0).endVertex();
-            wr.pos(-0.5, 0.0, 0.5).tex(0.0, 1.0).endVertex();
-            wr.pos(-0.5, 1.0, 0.5).tex(0.0, 0.0).endVertex();
-            wr.pos(-0.5, 1.0, -0.5).tex(1.0, 0.0).endVertex();
-            wr.pos(-0.5, 1.0, 0.5).tex(1.0, 1.0).endVertex();
-            wr.pos(0.5, 1.0, 0.5).tex(0.0, 1.0).endVertex();
-            wr.pos(0.5, 1.0, -0.5).tex(0.0, 0.0).endVertex();
-            wr.pos(-0.5, 1.0, -0.5).tex(1.0, 0.0).endVertex();
-            wr.pos(0.5, 0.0, -0.5).tex(1.0, 1.0).endVertex();
-            wr.pos(0.5, 0.0, 0.5).tex(0.0, 1.0).endVertex();
-            wr.pos(-0.5, 0.0, 0.5).tex(0.0, 0.0).endVertex();
-            Tessellator.getInstance().draw();
+            BlockRendererDispatcher blockrendererdispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
+            BlockPos pos = new BlockPos(x1, y1, z1);
+            IBlockState blockState = entityWorld.getBlockState(pos);
+            BlockPos pos1 = new BlockPos(x1, y1 + 1, z1);
+
+            WorldRenderer worldRenderer = Tessellator.getInstance().getWorldRenderer();
+            List<VertexFormatElement> elements = worldRenderer.getVertexFormat().getElements();
+//            elements.add(DefaultVertexFormats.TEX_2S);
+            blockrendererdispatcher.renderBlock(blockState, pos1, new RegionRenderCache(entityWorld, pos1.add(-1, -1, -1), pos1.add(1, 1, 1), 1), worldRenderer);
+//            elements.remove(DefaultVertexFormats.TEX_2S);
+
 
         }
     }
