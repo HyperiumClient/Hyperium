635a115673bdc94c5f2c4dd8ae3fd9dbc7e8c071
Mitchell Katz <sk1er@lythrim.net>
2018-06-06 17:06:12 -0400

    Fix update check

--- src/main/java/cc/hyperium/utils/UpdateUtils.java
+++ src/main/java/cc/hyperium/utils/UpdateUtils.java
@@ -2,9 +2,6 @@ package cc.hyperium.utils;
 
 import cc.hyperium.Metadata;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static cc.hyperium.installer.InstallerFrame.get;
 
 /**
@@ -16,18 +13,11 @@ public class UpdateUtils {
 
     public boolean isUpdated() {
         String versions_url = "https://raw.githubusercontent.com/HyperiumClient/Hyperium-Repo/master/installer/versions.json";
-
         JsonHolder json;
         try {
             json = new JsonHolder(get(versions_url));
             this.vJson = json;
-
-            String latest = json.optString("latest-stable");
-
-            Matcher remote = Pattern.compile("(?<version>\\d.+\\.\\d.+) B(?<build>\\d.+)").matcher(latest);
-            Matcher local = Pattern.compile("(?<version>\\d.+\\.\\d.+) - (Dev|Beta) B(?<build>\\d.+)").matcher(Metadata.getVersion());
-
-            return Integer.parseInt(local.group("build")) < Integer.parseInt(remote.group("build"));
+            return Metadata.getVersionID() >= json.optInt("latest-id");
         } catch (Exception e) {
             e.printStackTrace();
         }
