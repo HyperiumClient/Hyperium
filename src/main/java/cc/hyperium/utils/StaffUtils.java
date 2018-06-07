00045397430b7ffcfc0207c2b70c0b18e7d4b049
Amplifiable2 <gerritegberink3@gmail.com>
2018-06-07 19:52:46 +0200

    C H R O M A

--- src/main/java/cc/hyperium/utils/StaffUtils.java
+++ src/main/java/cc/hyperium/utils/StaffUtils.java
@@ -9,24 +9,30 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class StaffUtils {
-    private static final HashMap<UUID, ChatColor> STAFF_CACHE = new HashMap<>();
+    private static final HashMap<UUID, DotColour> STAFF_CACHE = new HashMap<>();
     public static boolean isStaff(UUID uuid) {
         return STAFF_CACHE.keySet().contains(uuid);
     }
 
-    public static ChatColor getColor(UUID uuid) {
+    public static DotColour getColor(UUID uuid) {
         return STAFF_CACHE.get(uuid);
     }
 
-    public static HashMap<UUID, ChatColor> getStaff() throws IOException {
-        HashMap<UUID, ChatColor> staff = new HashMap<>();
+    public static HashMap<UUID, DotColour> getStaff() throws IOException {
+        HashMap<UUID, DotColour> staff = new HashMap<>();
         String content = InstallerFrame.get("https://raw.githubusercontent.com/HyperiumClient/Hyperium-Repo/master/files/staff.json");
         JSONArray array = new JSONArray(content);
         for (int i = 0; i < array.length(); i++) {
             JSONObject item = array.getJSONObject(i);
             UUID uuid = UUID.fromString(item.getString("uuid"));
-            ChatColor color = ChatColor.valueOf(item.getString("color").toUpperCase());
-            staff.put(uuid, color);
+            String colourStr = item.getString("color").toUpperCase();
+            DotColour colour;
+            if (colourStr.equals("CHROMA")) {
+                colour = new DotColour(true, ChatColor.WHITE);
+            } else {
+                colour = new DotColour(false, ChatColor.valueOf(colourStr));
+            }
+            staff.put(uuid, colour);
         }
         return staff;
     }
@@ -34,5 +40,14 @@ public class StaffUtils {
     public static void clearCache() throws IOException {
         STAFF_CACHE.clear();
         STAFF_CACHE.putAll(getStaff());
+    }
+
+    public static class DotColour {
+        public boolean isChroma = false;
+        public ChatColor baseColour;
+        public DotColour(boolean isChroma, ChatColor baseColour) {
+            this.isChroma = isChroma;
+            this.baseColour = baseColour;
+        }
     }
 }
