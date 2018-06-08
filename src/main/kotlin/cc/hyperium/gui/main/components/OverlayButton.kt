96f1aef5d17bad00073c04ebcba3eee8e9b9fd17
VRCube <vrcproxz@gmail.com>
2018-06-08 07:23:21 +0200

    separate button and label #kotlin4life

--- src/main/kotlin/cc/hyperium/gui/main/components/OverlayButton.kt
+++ src/main/kotlin/cc/hyperium/gui/main/components/OverlayButton.kt
@@ -2,11 +2,7 @@ package cc.hyperium.gui.main.components
 
 import org.lwjgl.input.Mouse
 
-class OverlayButton(label: String, val callback: Runnable) : OverlayComponent() {
-    init {
-        this.label = label
-    }
-
+class OverlayButton(label: String, val callback: Runnable) : OverlayLabel(label) {
     override fun handleMouseInput(mouseX: Int, mouseY: Int, overlayX: Int, overlayY: Int, w: Int, h: Int) {
         if (Mouse.isButtonDown(0) && mouseX >= overlayX + w - 30 && mouseX <= overlayX + w - 5 && mouseY >= overlayY + 5 && mouseY <= overlayY + h - 5)
             callback.run()
