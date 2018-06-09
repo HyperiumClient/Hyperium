a36648231d0adce7e0ea8ccdec777dfacf771401
Conor Byrne <conorbyrne06@icloud.com>
2018-06-09 22:44:22 +0100

    Re-wrote Wings & AngelWings

---
+++ src/main/java/cc/hyperium/cosmetics/Wings/WingsRenderer.java
@@ -0,0 +1,90 @@
+package cc.hyperium.cosmetics.Wings;
+
+
+import cc.hyperium.Hyperium;
+import cc.hyperium.cosmetics.AbstractCosmetic;
+import cc.hyperium.event.InvokeEvent;
+import cc.hyperium.event.RenderPlayerEvent;
+import net.minecraft.client.*;
+import net.minecraft.util.*;
+import net.minecraft.client.model.*;
+import net.minecraft.entity.player.*;
+import org.lwjgl.opengl.*;
+
+public class WingsRenderer extends ModelBase
+{
+    private Minecraft mc;
+    private ResourceLocation location = new ResourceLocation("textures/cosmetics/wings/angelwings.png");
+    private ModelRenderer wing;
+    private ModelRenderer wingTip;
+    private boolean playerUsesFullHeight;
+    private WingsCosmetic wingsCosmetic = new WingsCosmetic();
+    AbstractCosmetic abstractCosmetic = new WingsCosmetic();
+
+    public WingsRenderer() {
+        this.mc = Minecraft.getMinecraft();
+        this.setTextureOffset("wing.bone", 0, 0);
+        this.setTextureOffset("wing.skin", -10, 8);
+        this.setTextureOffset("wingtip.bone", 0, 5);
+        this.setTextureOffset("wingtip.skin", -10, 18);
+        (this.wing = new ModelRenderer((ModelBase)this, "wing")).setTextureSize(30, 30);
+        this.wing.setRotationPoint(-2.0f, 0.0f, 0.0f);
+        this.wing.addBox("bone", -10.0f, -1.0f, -1.0f, 10, 2, 2);
+        this.wing.addBox("skin", -10.0f, 0.0f, 0.5f, 10, 0, 10);
+        (this.wingTip = new ModelRenderer((ModelBase)this, "wingtip")).setTextureSize(30, 30);
+        this.wingTip.setRotationPoint(-10.0f, 0.0f, 0.0f);
+        this.wingTip.addBox("bone", -10.0f, -0.5f, -0.5f, 10, 1, 1);
+        this.wingTip.addBox("skin", -10.0f, 0.0f, 0.5f, 10, 0, 10);
+        this.wing.addChild(this.wingTip);
+    }
+
+    @InvokeEvent
+    public void onRenderPlayer(final RenderPlayerEvent event) {
+        final EntityPlayer player = event.getEntity();
+        if (abstractCosmetic.isPurchasedBy(mc.thePlayer.getUniqueID()) || Hyperium.INSTANCE.isDevEnv() &&  player.equals((Object)this.mc.thePlayer) && !player.isInvisible()) {
+            this.renderWings(player, event.getPartialTicks());
+        }
+    }
+
+    private void renderWings(final EntityPlayer player, final float partialTicks) {
+        final double scale = this.wingsCosmetic.scale / 100.0;
+        final double rotate = this.interpolate(player.prevRenderYawOffset, player.renderYawOffset, partialTicks);
+        GL11.glPushMatrix();
+        GL11.glScaled(-scale, -scale, scale);
+        GL11.glRotated(180.0 + rotate, 0.0, 1.0, 0.0);
+        GL11.glTranslated(0.0, -(this.playerUsesFullHeight ? 1.45 : 1.25) / scale, 0.0);
+        GL11.glTranslated(0.0, 0.0, 0.2 / scale);
+        if (player.isSneaking()) {
+            GL11.glTranslated(0.0, 0.125 / scale, 0.0);
+        }
+        final float[] colors = this.wingsCosmetic.getColours();
+        GL11.glColor3f(colors[0], colors[1], colors[2]);
+        this.mc.getTextureManager().bindTexture(this.location);
+
+        for (int j = 0; j < 2; ++j) {
+            GL11.glEnable(2884);
+            final float f11 = System.currentTimeMillis() % 1000L / 1000.0f * 3.1415927f * 2.0f;
+            this.wing.rotateAngleX = (float)Math.toRadians(-80.0) - (float)Math.cos(f11) * 0.2f;
+            this.wing.rotateAngleY = (float)Math.toRadians(20.0) + (float)Math.sin(f11) * 0.4f;
+            this.wing.rotateAngleZ = (float)Math.toRadians(20.0);
+            this.wingTip.rotateAngleZ = -(float)(Math.sin(f11 + 2.0f) + 0.5) * 0.75f;
+            this.wing.render(0.0625f);
+            GL11.glScalef(-1.0f, 1.0f, 1.0f);
+            if (j == 0) {
+                GL11.glCullFace(1028);
+            }
+        }
+        GL11.glCullFace(1029);
+        GL11.glDisable(2884);
+        GL11.glColor3f(255.0f, 255.0f, 255.0f);
+        GL11.glPopMatrix();
+    }
+
+    private float interpolate(final float yaw1, final float yaw2, final float percent) {
+        float f = (yaw1 + (yaw2 - yaw1) * percent) % 360.0f;
+        if (f < 0.0f) {
+            f += 360.0f;
+        }
+        return f;
+    }
+}
