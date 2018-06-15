e4ce75ac384887f5c0ef6bca6a587516e1831ba7
Mitchell Katz <sk1er@lythrim.net>
2018-06-15 08:12:31 -0400

    Remove print

--- src/main/java/cc/hyperium/purchases/HyperiumPurchase.java
+++ src/main/java/cc/hyperium/purchases/HyperiumPurchase.java
@@ -46,7 +46,7 @@ public class HyperiumPurchase {
                     if (parse1 != null)
                         this.purchases.add(parse1);
                 } catch (Exception wtf) {
-                    wtf.printStackTrace();
+//                    wtf.printStackTrace();
                 }
         }
     }
