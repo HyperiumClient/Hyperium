1790ba23f6a17313d7eb97d4a57ded1b24de9748
Amplifiable2 <37980280+Amplifiable2@users.noreply.github.com>
2018-06-16 07:32:59 +0200

    Port OverlayChecker to java (the isInstance seems weird to me but it was the same in kotlin version) (#313)

---
+++ src/main/java/cc/hyperium/internal/addons/OverlayChecker.java
@@ -0,0 +1,17 @@
+package cc.hyperium.internal.addons;
+
+import cc.hyperium.gui.main.HyperiumOverlay;
+import cc.hyperium.internal.addons.misc.AddonLoadException;
+
+public class OverlayChecker {
+  public static void checkOverlayField(String value) throws AddonLoadException {
+    try {
+      Class<?> originClass = Class.forName(value);
+      if (HyperiumOverlay.class.isInstance(originClass)) {
+        throw new AddonLoadException("overlay has to be an instance of HyperiumOverlay");
+      }
+    } catch (ClassNotFoundException e) {
+      throw new AddonLoadException("Overlay class doesn't exist");
+    }
+  }
+}
