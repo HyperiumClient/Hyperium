f8bd47ef58d69027572987da03047e2f82e39fd2
Sk1er <sk1er@sk1er.club>
2018-06-23 11:56:59 -0700

    Start particle aura

---
+++ src/main/java/cc/hyperium/handlers/handlers/ParticleAuraHandler.java
@@ -0,0 +1,16 @@
+package cc.hyperium.handlers.handlers;
+
+import cc.hyperium.event.InvokeEvent;
+import cc.hyperium.event.RenderPlayerEvent;
+
+/**
+ * Created by mitchellkatz on 6/23/18. Designed for production use on Sk1er.club
+ */
+public class ParticleAuraHandler {
+
+
+    @InvokeEvent
+    public void renderPlayer(RenderPlayerEvent event) {
+
+    }
+}
