1a5282a704b76444ab8a7e61e401e1bb22785bb3
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-06-24 13:32:10 +0100

    Changed to endermite
    ignore the change to 9y0
    i committed to the wrong file

--- src/main/java/cc/hyperium/utils/eastereggs/eastereggplayerdata/EasterEggMatt.java
+++ src/main/java/cc/hyperium/utils/eastereggs/eastereggplayerdata/EasterEggMatt.java
@@ -18,7 +18,7 @@
 package cc.hyperium.utils.eastereggs.eastereggplayerdata;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.monster.EntityEndermite;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.PotionEffect;
 
@@ -29,7 +29,7 @@ import net.minecraft.potion.PotionEffect;
  */
 public class EasterEggMatt {
     public static Entity renderEvent(EntityPlayer player) {
-        EntityPig disguise = new EntityPig(player.worldObj);
+        EntityEndermite disguise = new EntityEndermite(player.worldObj);
         disguise.ticksExisted = 1;
         disguise.posX = player.posX;
         disguise.lastTickPosX = player.lastTickPosX;
