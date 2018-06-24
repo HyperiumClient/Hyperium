5b02e1a10516970f07ef968d29ed43a2a49a77d3
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-06-24 22:41:52 +0100

    license and easteregg config thingy

--- src/main/java/cc/hyperium/config/Settings.java
+++ src/main/java/cc/hyperium/config/Settings.java
@@ -1,3 +1,20 @@
+/*
+ *     Copyright (C) 2018  Hyperium <https://hyperium.cc/>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package cc.hyperium.config;
 
 import cc.hyperium.GuiStyle;
@@ -300,6 +317,12 @@ public class Settings {
     @ConfigOpt
     @ToggleSetting(name = "Show ChromaHUD", category = INTEGRATIONS)
     public static boolean SHOW_CHROMAHUD = true;
+
+
+    @ConfigOpt
+    @ToggleSetting(name = "Super Secret Settings v2", category = MISC)
+    public static boolean SUPERSECRETSETTINGSV2 = true;
+
 
     public static void register() {
         Hyperium.CONFIG.register(new Settings()); // values r static soo whatever
