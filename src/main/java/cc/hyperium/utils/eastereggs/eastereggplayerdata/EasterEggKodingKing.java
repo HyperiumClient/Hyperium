6d0707593e8dbdbef97e8cf9096ced7384a52cb9
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-06-24 22:00:03 +0100

    changed to squid

--- src/main/java/cc/hyperium/utils/eastereggs/eastereggplayerdata/EasterEggKodingKing.java
+++ src/main/java/cc/hyperium/utils/eastereggs/eastereggplayerdata/EasterEggKodingKing.java
@@ -18,7 +18,7 @@
 package cc.hyperium.utils.eastereggs.eastereggplayerdata;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.PotionEffect;
 
@@ -29,7 +29,7 @@ import net.minecraft.potion.PotionEffect;
  */
 public class EasterEggKodingKing {
     public static Entity renderEvent(EntityPlayer player) {
-        EntityPig disguise = new EntityPig(player.worldObj);
+        EntitySquid disguise = new EntitySquid(player.worldObj);
         disguise.ticksExisted = 1;
         disguise.posX = player.posX;
         disguise.lastTickPosX = player.lastTickPosX;
