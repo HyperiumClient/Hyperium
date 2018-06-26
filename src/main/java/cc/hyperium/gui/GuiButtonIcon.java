5a76a768e0ec6a794086b018bcc3cff7fb89c97e
Mitchell Katz <sk1er@lythrim.net>
2018-06-26 15:02:17 -0400

    Fix button issues

--- src/main/java/cc/hyperium/gui/GuiButtonIcon.java
+++ src/main/java/cc/hyperium/gui/GuiButtonIcon.java
@@ -30,7 +30,7 @@ public class GuiButtonIcon extends GuiButton {
     private boolean outline;
 
     public GuiButtonIcon(int buttonID, ResourceLocation icon, int xPos, int yPos, int sprite, float scale) {
-        super(buttonID, xPos, yPos, 52, 52, "");
+        super(buttonID, xPos, yPos, (int) (52 * scale), (int) (52 * scale), "");
         this.icon = icon;
         this.sprite = sprite;
         this.scale = scale;
@@ -48,23 +48,25 @@ public class GuiButtonIcon extends GuiButton {
      * Draws this button to the screen.
      */
     public void drawButton(Minecraft mc, int mouseX, int mouseY) {
-        if (this.visible) {
+        if (visible) {
+            int width = 52;
+            int height = 52;
             mc.getTextureManager().bindTexture(icon);
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            boolean flag = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+            boolean flag = mouseX >= xPosition && mouseY >= yPosition && mouseX < xPosition + width && mouseY < yPosition + height;
             // todo if flag glcolour
             GL11.glPushMatrix();
             GL11.glScalef(scale, scale, scale);
             GlStateManager.enableBlend();
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
-            GL11.glTranslatef(this.xPosition / scale + ((float) width) / scale, this.yPosition / scale + ((float) height) / scale, 0);
+            GL11.glTranslatef(xPosition / scale + this.width/scale/2, yPosition / scale + this.height / scale / 2, 0);
             GlStateManager.color(0, 0, 0, 1.0F);
             float mag = 1.25F;
             GlStateManager.scale(mag, mag, mag);
-            this.drawTexturedModalRect(-width / 2, -height / 2, 52 * sprite, 0, this.width, this.height);
+            drawTexturedModalRect(-width / 2, -height / 2, 52 * sprite, 0, width,height);
             GlStateManager.scale(1.0F / mag, 1.0F / mag, 1.0F / mag);
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            this.drawTexturedModalRect(-width / 2, -height / 2, 52 * sprite, 0, this.width, this.height);
+            drawTexturedModalRect(-width / 2, -height / 2, 52 * sprite, 0, width,height);
 
             GlStateManager.disableBlend();
             GL11.glPopMatrix();
