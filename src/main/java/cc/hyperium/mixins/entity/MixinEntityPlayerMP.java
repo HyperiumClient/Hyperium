07eb08a2e2f9c6ef87f39d18d94f7c19649bc535
Gerrit Egberink <gerritegberink3@gmail.com>
2018-06-30 09:46:17 +0200

    Add events (trello)

---
+++ src/main/java/cc/hyperium/mixins/entity/MixinEntityPlayerMP.java
@@ -0,0 +1,19 @@
+package cc.hyperium.mixins.entity;
+
+import cc.hyperium.event.EventBus;
+import cc.hyperium.event.LivingDeathEvent;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.DamageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityPlayerMP.class)
+public class MixinEntityPlayerMP {
+  @Inject(method = "onDeath", at = @At("HEAD"))
+  private void onDeath(DamageSource source, CallbackInfo ci) {
+    EventBus.INSTANCE.post(new LivingDeathEvent((EntityLivingBase) (Object) this, source));
+  }
+}
