a9d65c4cb4adeacb38bb12776644f76178aa3308
CoalCoding <coalores@gmail.com>
2018-07-07 01:16:08 +0100

    Moved MixinServerList code to HyperiumServerList

--- src/main/java/cc/hyperium/mixins/client/multiplayer/MixinServerList.java
+++ src/main/java/cc/hyperium/mixins/client/multiplayer/MixinServerList.java
@@ -1,19 +1,15 @@
 package cc.hyperium.mixins.client.multiplayer;
 
+import cc.hyperium.mixinsimp.client.multiplayer.HyperiumServerList;
+import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.multiplayer.ServerList;
-import net.minecraft.nbt.CompressedStreamTools;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-
-import java.io.File;
-import java.util.List;
 
 @Mixin(ServerList.class)
 public class MixinServerList {
@@ -27,119 +23,50 @@ public class MixinServerList {
     @Final
     private Minecraft mc;
 
+    private HyperiumServerList hyperiumServerList = new HyperiumServerList();
+
     @Overwrite
     public static void func_147414_b(ServerData p_147414_0_) {
-        //TODO: Move to new class.
-        ServerList serverlist = new ServerList(Minecraft.getMinecraft());
-        serverlist.loadServerList();
-
-        for (int i = 0; i < serverlist.countServers(); ++i) {
-            ServerData serverdata = serverlist.getServerData(i);
-
-            if (serverdata.serverName.equals(p_147414_0_.serverName) && serverdata.serverIP.equals(p_147414_0_.serverIP)) {
-                serverlist.func_147413_a(i, p_147414_0_);
-                break;
-            }
-        }
-
-        serverlist.saveServerList();
+        HyperiumServerList.func_147414_b(p_147414_0_);
     }
 
     @Overwrite
     public void loadServerList() {
-        try {
-            this.servers.clear();
-            NBTTagCompound nbttagcompound = CompressedStreamTools.read(new File(this.mc.mcDataDir, "servers.dat"));
-
-            if (nbttagcompound == null) {
-                return;
-            }
-
-            NBTTagList nbttaglist = nbttagcompound.getTagList("servers", 10);
-
-            for (int i = 0; i < nbttaglist.tagCount(); ++i) {
-                this.servers.add(ServerData.getServerDataFromNBTCompound(nbttaglist.getCompoundTagAt(i)));
-            }
-        } catch (Exception exception) {
-            logger.error((String) "Couldn\'t load server list", (Throwable) exception);
-            System.out.println("Load server list error");
-
-        }
+        hyperiumServerList.loadServers(this.servers,logger,this.mc);
     }
 
     @Overwrite
     public void saveServerList() {
-        try {
-            NBTTagList nbttaglist = new NBTTagList();
-
-            for (ServerData serverdata : this.servers) {
-                nbttaglist.appendTag(serverdata.getNBTCompound());
-            }
-
-            NBTTagCompound nbttagcompound = new NBTTagCompound();
-            nbttagcompound.setTag("servers", nbttaglist);
-            CompressedStreamTools.safeWrite(nbttagcompound, new File(this.mc.mcDataDir, "servers.dat"));
-        } catch (Exception exception) {
-            logger.error((String) "Couldn\'t save server list", (Throwable) exception);
-            System.out.println("Save server list error");
-        }
-
+        hyperiumServerList.saveServerList(this.servers,logger,this.mc);
     }
 
     @Overwrite
     public ServerData getServerData(int p_78850_1_) {
-        try {
-            return (ServerData) this.servers.get(p_78850_1_);
-        } catch (Exception e) {
-            System.out.println("GetServer Data error 1");
-            e.printStackTrace();
-        }
-        return null;
+        return hyperiumServerList.getServerData(this.servers,p_78850_1_);
     }
 
     @Overwrite
     public void removeServerData(int p_78851_1_) {
-        try {
-            this.servers.remove(p_78851_1_);
-        } catch (Exception e) {
-            System.out.println("Remove server data error");
-        }
+        hyperiumServerList.removeServerData(this.servers,p_78851_1_);
     }
 
     @Overwrite
     public void addServerData(ServerData p_78849_1_) {
-        try {
-            this.servers.add(p_78849_1_);
-        } catch (Exception e) {
-            System.out.println("Add server data error");
-
-        }
+       hyperiumServerList.addServerData(this.servers,p_78849_1_);
     }
 
     @Overwrite
     public int countServers() {
-        return this.servers.size();
+        return hyperiumServerList.countServers(this.servers);
     }
 
     @Overwrite
     public void swapServers(int p_78857_1_, int p_78857_2_) {
-        try {
-            ServerData serverdata = this.getServerData(p_78857_1_);
-            this.servers.set(p_78857_1_, this.getServerData(p_78857_2_));
-            this.servers.set(p_78857_2_, serverdata);
-            this.saveServerList();
-        } catch (Exception e) {
-            System.out.println("Swap servers error");
-
-        }
+        hyperiumServerList.swapServers(this.servers,logger,this.mc,p_78857_1_,p_78857_2_);
     }
 
     @Overwrite
     public void func_147413_a(int p_147413_1_, ServerData p_147413_2_) {
-        try {
-            this.servers.set(p_147413_1_, p_147413_2_);
-        } catch (Exception e) {
-            System.out.println("func_147413_a server data error");
-        }
+        hyperiumServerList.func_147413_a(this.servers,p_147413_1_,p_147413_2_);
     }
 }
