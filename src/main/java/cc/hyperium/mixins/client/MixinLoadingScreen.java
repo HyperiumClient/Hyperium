25aa3a1277866dd4cf8cfd3569a2a534807ffb38
CoalCoding <coalores@gmail.com>
2018-07-12 20:57:44 +0100

    Fixed white screen when loading world.

---
+++ src/main/java/cc/hyperium/mixins/client/MixinLoadingScreen.java
@@ -0,0 +1,19 @@
+package cc.hyperium.mixins.client;
+
+import net.minecraft.client.LoadingScreenRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LoadingScreenRenderer.class)
+public class MixinLoadingScreen {
+
+    @Inject(method = "setLoadingProgress",at=@At("HEAD"))
+    public void setLoadingProgress(int progress, CallbackInfo ci){
+        GlStateManager.disableTexture2D();
+        GlStateManager.enableTexture2D();
+    }
+
+}
