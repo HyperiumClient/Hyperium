22bb64c8a9eea95a38a4f645be4039c91526f175
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-07-16 07:31:29 +0100

    Create AbstractAddon.java
    totally not a skid of AbstractMod
    shhhh

---
+++ src/main/java/cc/hyperium/addons/AbstractAddon.java
@@ -0,0 +1,83 @@
+/*
+ *     Copyright (C) 2018  Hyperium <https://hyperium.cc/>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cc.hyperium.addons;
+
+import org.apache.commons.lang3.StringUtils;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public abstract class AbstractAddon {
+    public abstract AbstractAddon init();
+    
+    public abstract Metadata getAddonMetadata();
+    
+    public class Metadata {
+        private final AbstractAddon addon;
+        private final String author;
+        private final String name;
+        private final String version;
+
+        private String displayName;
+
+        public Metadata(AbstractAddon addon, String name) {
+            this(mod, name, "1.0");
+        }
+
+        public Metadata(AbstractAddon addon, String name, String version) {
+            this(addon, name, version, "");
+        }
+
+        public Metadata(AbstractMod mod, String name, String version, String author) {
+            checkNotNull(addon, "Addon instance cannot be null");
+            checkArgument(!StringUtils.isEmpty(name), "Name cannot be null or empty (" + name + ")");
+            checkArgument(!StringUtils.isEmpty(version), "Version cannot be null or empty (" + version + ")");
+            checkNotNull(author, "Author cannot be null (" + author + ")");
+
+            this.addon = addon;
+            this.name = name;
+            this.author = author;
+            this.version = version;
+            this.displayName = name;
+        }
+
+        public AbstractAddon getAddon() {
+            return this.addon;
+        }
+
+        public String getName() {
+            return this.name != null ? this.name : "";
+        }
+
+        public String getVersion() {
+            return this.version;
+        }
+
+        public String getAuthor() {
+            return this.author != null ? this.author : "";
+        }
+
+        public String getDisplayName() {
+            return this.displayName != null ? this.displayName : getName();
+        }
+
+        public void setDisplayName(String name) {
+            this.displayName = name;
+        }
+    }
+}
