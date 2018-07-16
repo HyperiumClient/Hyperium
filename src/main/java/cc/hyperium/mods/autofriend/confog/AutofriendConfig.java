931ef9b779e725900bfe4db09dc615d23264974e
Conor Byrne <conorbyrne06@icloud.com>
2018-07-16 20:37:02 +0100

    Fixed issue where Autofriend's overlay wouldn't display anything

--- src/main/java/cc/hyperium/mods/autofriend/confog/AutofriendConfig.java
+++ src/main/java/cc/hyperium/mods/autofriend/confog/AutofriendConfig.java
@@ -6,12 +6,4 @@ import cc.hyperium.config.ToggleSetting;
 
 public class AutofriendConfig {
 
-    @ConfigOpt
-    @ToggleSetting(name = "Enabled", category = Category.AUTOFRIEND, mods = true)
-    public static boolean toggle = true;
-
-    @ConfigOpt
-    @ToggleSetting(name = "Show Friend Messages", category = Category.AUTOFRIEND, mods = true)
-    public static boolean messages = true;
-
 }
