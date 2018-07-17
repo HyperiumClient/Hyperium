00bdbc639de28d0a2aa41bc64572391bf6c50a4a
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-07-17 08:20:14 +0100

    customrp attempt 2 (#384)
    * Create readme.txt
    
    * Create CustomRP.java
    
    * Create Config.java
    
    * Create Overlay.java
    
    * Create AddonUpdateChecker.java
    
    * Create Mode.java
    
    * Create RichPresenceUpdater.java
    
    * Create addon.json
    
    * add customrp
    
    * plz fix the issue
    
    * Create AbstractAddon.java
    
    totally not a skid of AbstractMod
    shhhh
    
    * Add files via upload
    
    * Add files via upload
    
    * remove update checker loader
    
    * buh bye update checker
    
    * remove update checker import
    
    why am i so bad
    
    * loading should work now
    
    * update checker buy bye
    
    how am i so bad
    
    * more badness on my part
    
    * how much did i use update checker?
    
    * Update AbstractAddon.java
    
    * moved compatibility
    
    * Update CustomRP.java
    
    * load internal addons kthx

--- src/main/java/cc/hyperium/addons/InternalAddons.java
+++ src/main/java/cc/hyperium/addons/InternalAddons.java
@@ -1,12 +1,20 @@
 package cc.hyperium.addons;
 
+import cc.hyperium.addons.customrp.CustomRP;
+import cc.hyperium.event.EventBus;
+import cc.hyperium.mods.glintcolorizer.GlintColorizer;
+
 public class InternalAddons {
-
-
-    //Put loading stuff in here
-
+    //CustomRP
+    private final CustomRP customrp;
 
     public InternalAddons() {
+        //CustomRP
+        this.customrp = ((CustomRP) new CustomRP().init());
+    }
 
+    //CustomRP
+    public CustomRP getCustomRP() {
+        return customrp;
     }
 }
