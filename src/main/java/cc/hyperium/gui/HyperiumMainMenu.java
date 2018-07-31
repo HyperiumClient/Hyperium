0483c9e21874039643fe9415099b84a89210232b
SiroQ <siro0644@gmail.com>
2018-07-31 11:23:20 +0900

    Is there language button?
    I think no.

--- src/main/java/cc/hyperium/gui/HyperiumMainMenu.java
+++ src/main/java/cc/hyperium/gui/HyperiumMainMenu.java
@@ -365,10 +365,6 @@ public class HyperiumMainMenu extends GuiScreen implements GuiYesNoCallback {
             this.mc.displayGuiScreen(new GuiOptions(this, this.mc.gameSettings));
         }
 
-        if (button.id == 5) {
-            this.mc.displayGuiScreen(new GuiLanguage(this, this.mc.gameSettings, this.mc.getLanguageManager()));
-        }
-
         if (button.id == 1) {
             this.mc.displayGuiScreen(new GuiSelectWorld(this));
         }
