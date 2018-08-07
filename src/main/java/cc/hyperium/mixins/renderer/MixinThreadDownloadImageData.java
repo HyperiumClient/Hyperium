0c10d18ab99011958df5e84e0bb656fc7b91c766
Mitchell Katz <sk1er@lythrim.net>
2018-08-07 15:20:37 -0400

    Remove textures being deleted

--- src/main/java/cc/hyperium/mixins/renderer/MixinThreadDownloadImageData.java
+++ src/main/java/cc/hyperium/mixins/renderer/MixinThreadDownloadImageData.java
@@ -9,9 +9,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -45,8 +42,8 @@ public abstract class MixinThreadDownloadImageData extends SimpleTexture {
         hyperiumThreadDownloadImageData.loadTextureFromServer(imageUrl, cacheFile, imageBuffer, (ThreadDownloadImageData) (Object) this,textureLocation);
     }
 
-    @Inject(method = "checkTextureUploaded", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureUtil;uploadTextureImage(ILjava/awt/image/BufferedImage;)I", shift = At.Shift.AFTER))
-    public void test(CallbackInfo info) {
-        setBufferedImage(null);
-    }
+//    @Inject(method = "checkTextureUploaded", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureUtil;uploadTextureImage(ILjava/awt/image/BufferedImage;)I", shift = At.Shift.AFTER))
+//    public void test(CallbackInfo info) {
+//        setBufferedImage(null);
+//    }
 }
