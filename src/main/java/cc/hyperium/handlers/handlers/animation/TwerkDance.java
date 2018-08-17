a9ee19b1d0fd2d9c445f9880f0417d880d7f36e1
Mitchell Katz <sk1er@lythrim.net>
2018-08-16 21:56:15 -0400

    Move to remote data

--- src/main/java/cc/hyperium/handlers/handlers/animation/TwerkDance.java
+++ src/main/java/cc/hyperium/handlers/handlers/animation/TwerkDance.java
@@ -1,10 +1,10 @@
 package cc.hyperium.handlers.handlers.animation;
 
 import cc.hyperium.utils.JsonHolder;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class TwerkDance extends AnimatedDance {
 
@@ -12,8 +12,8 @@ public class TwerkDance extends AnimatedDance {
     //THE EPITOME OF PAIN. FOR YOUR OWN GOOD, DO NOT ATTEMPT
     public JsonHolder getData() {
         try {
-//            holder = new JsonHolder(IOUtils.toString(new URL("https://static.sk1er.club/hyperium/twerk.json")));
-            return new JsonHolder(FileUtils.readFileToString(new File("twerk.json")));
+            return new JsonHolder(IOUtils.toString(new URL("https://static.sk1er.club/hyperium/twerk.json")));
+//            return new JsonHolder(FileUtils.readFileToString(new File("twerk.json")));
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/cc/hyperium/handlers/handlers/animation/fortnite/FortniteDefaultDance.java
+++ src/main/java/cc/hyperium/handlers/handlers/animation/fortnite/FortniteDefaultDance.java
@@ -2,10 +2,10 @@ package cc.hyperium.handlers.handlers.animation.fortnite;
 
 import cc.hyperium.handlers.handlers.animation.AnimatedDance;
 import cc.hyperium.utils.JsonHolder;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class FortniteDefaultDance extends AnimatedDance {
 
@@ -13,8 +13,8 @@ public class FortniteDefaultDance extends AnimatedDance {
     @Override
     public JsonHolder getData() {
         try {
-//            return new JsonHolder(IOUtils.toString(new URL("https://static.sk1er.club/hyperium/fortnite_dance.json")));
-            return new JsonHolder(FileUtils.readFileToString(new File("fortnite_dance.json")));
+            return new JsonHolder(IOUtils.toString(new URL("https://static.sk1er.club/hyperium/fortnite_dance.json")));
+//            return new JsonHolder(FileUtils.readFileToString(new File("fortnite_dance.json")));
         } catch (IOException e) {
             e.printStackTrace();
         }
