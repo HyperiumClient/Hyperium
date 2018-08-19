48a33bb6a2c72a1811d4551a78b9ae92f9702c33
Mitchell Katz <sk1er@lythrim.net>
2018-08-19 00:27:03 -0400

    Update branding

--- src/main/kotlin/cc/hyperium/Metadata.kt
+++ src/main/kotlin/cc/hyperium/Metadata.kt
@@ -30,7 +30,7 @@ val MODID
 
 val VERSION
     @JvmName("getVersion")
-    get() = "1.0 - Beta B23"
+    get() = "1.0 - Beta B23.1"
 
 val AUTHORS
     @JvmName("getAuthors")
@@ -41,7 +41,7 @@ val AUTHORS
  */
 val VERSION_ID
     @JvmName("getVersionID")
-    get() = 34
+    get() = 35
 
 val ICON_BASE64
     @JvmName("getIconBase64")
