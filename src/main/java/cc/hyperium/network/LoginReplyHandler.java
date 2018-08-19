d8c645e18d05a0f2296bf6872675143e036c6dee
Mitchell Katz <sk1er@lythrim.net>
2018-08-19 00:57:56 -0400

    Update location

--- src/main/java/cc/hyperium/network/LoginReplyHandler.java
+++ src/main/java/cc/hyperium/network/LoginReplyHandler.java
@@ -2,10 +2,16 @@ package cc.hyperium.network;
 
 import cc.hyperium.Hyperium;
 import cc.hyperium.commands.BaseCommand;
+import cc.hyperium.config.Settings;
 import cc.hyperium.netty.NettyClient;
 import cc.hyperium.netty.packet.PacketHandler;
 import cc.hyperium.netty.packet.PacketType;
 import cc.hyperium.netty.packet.packets.clientbound.LoginReplyPacket;
+import cc.hyperium.netty.packet.packets.serverbound.ServerCrossDataPacket;
+import cc.hyperium.netty.packet.packets.serverbound.UpdateLocationPacket;
+import cc.hyperium.utils.JsonHolder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ServerData;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -15,6 +21,16 @@ public class LoginReplyHandler implements PacketHandler<LoginReplyPacket> {
 
     @Override
     public void handle(LoginReplyPacket loginReplyPacket) {
+        ServerData currentServerData = Minecraft.getMinecraft().getCurrentServerData();
+        if (currentServerData != null) {
+            NettyClient client = NettyClient.getClient();
+            if (client != null) {
+                client.write(UpdateLocationPacket.build("Other"));
+                if (Settings.SEND_SERVER)
+                    client.write(ServerCrossDataPacket.build(new JsonHolder().put("internal", true).put("server_update", currentServerData.serverIP)));
+            }
+
+        }
         if (loginReplyPacket.isAdmin()) {
             Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().registerCommand(new BaseCommand() {
                 @Override
