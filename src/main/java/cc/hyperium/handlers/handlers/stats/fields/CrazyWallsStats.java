d18c07eaea32825403a43c003dc9dd7fc08bef24
Mitchell Katz <sk1er@lythrim.net>
2018-08-23 16:17:04 -0400

    Even more stats

--- src/main/java/cc/hyperium/handlers/handlers/stats/fields/CrazyWallsStats.java
+++ src/main/java/cc/hyperium/handlers/handlers/stats/fields/CrazyWallsStats.java
@@ -1,6 +1,10 @@
 package cc.hyperium.handlers.handlers.stats.fields;
 
 import cc.hyperium.handlers.handlers.stats.AbstractHypixelStats;
+import cc.hyperium.handlers.handlers.stats.display.StatsDisplayItem;
+import club.sk1er.website.api.requests.HypixelApiPlayer;
+
+import java.util.List;
 
 public class CrazyWallsStats extends AbstractHypixelStats {
     @Override
@@ -11,5 +15,13 @@ public class CrazyWallsStats extends AbstractHypixelStats {
     @Override
     public String getName() {
         return "Crazy Walls";
+    }
+
+
+    //I checked the api, there isn't much other useful info
+    @Override
+    public List<StatsDisplayItem> getDeepStats(HypixelApiPlayer player) {
+
+        return super.getPreview(player);
     }
 }
--- src/main/java/club/sk1er/website/utils/WebsiteUtils.java
+++ src/main/java/club/sk1er/website/utils/WebsiteUtils.java
@@ -3,8 +3,70 @@ package club.sk1er.website.utils;
 import com.google.gson.JsonObject;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 public class WebsiteUtils {
+
+    public static final List<String> blitz_kits = new ArrayList<>();
+    public static final TreeMap<Integer, String> numerals = new TreeMap<>();
+
+    static {
+        numerals.put(100, "C");
+        numerals.put(90, "XC");
+        numerals.put(50, "L");
+        numerals.put(40, "XL");
+        numerals.put(10, "X");
+        numerals.put(9, "IX");
+        numerals.put(5, "V");
+        numerals.put(4, "IV");
+        numerals.put(1, "I");
+        blitz_kits.add("Archer");
+        blitz_kits.add("Arachnologist");
+        blitz_kits.add("Astronaut");
+        blitz_kits.add("Armorer");
+        blitz_kits.add("Creeper Tamer");
+        blitz_kits.add("Baker");
+        blitz_kits.add("Blaze");
+        blitz_kits.add("Farmer");
+        blitz_kits.add("Fisherman");
+        blitz_kits.add("Florist");
+        blitz_kits.add("Golem");
+        blitz_kits.add("Horsetamer");
+        blitz_kits.add("Hunter");
+        blitz_kits.add("Knight");
+        blitz_kits.add("Meat Master");
+        blitz_kits.add("Necromancer");
+        blitz_kits.add("Paladin");
+        blitz_kits.add("Pigman");
+        blitz_kits.add("Red Dragon");
+        blitz_kits.add("Reaper");
+        blitz_kits.add("Rogue");
+        blitz_kits.add("Scout");
+        blitz_kits.add("Shadow Knight");
+        blitz_kits.add("Slimey Slime");
+        blitz_kits.add("Snowman");
+        blitz_kits.add("Speleologist");
+        blitz_kits.add("Tim");
+        blitz_kits.add("Toxicologist");
+        blitz_kits.add("Troll");
+        blitz_kits.add("Wolftamer");
+        blitz_kits.add("Hype Train");
+        blitz_kits.add("Jockey");
+
+    }
+
+    public static String numeral(int amount) {
+        try {
+            int l = numerals.floorKey(amount);
+            if (l == amount)
+                return numerals.get(l);
+            return numerals.get(l) + numeral(amount - l);
+        } catch (Exception e) {
+            return "-";
+        }
+    }
 
     public static String comma(Number number) {
         DecimalFormat formatter = new DecimalFormat("#,###");
