c9d7980a60e3e8b82dfe29887e0d5f211aefcc9f
Koding King <djitechdude@gmail.com>
2018-08-24 18:02:40 +1000

    Finished mega walls.

--- src/main/java/cc/hyperium/handlers/handlers/stats/fields/BuildBattleStats.java
+++ src/main/java/cc/hyperium/handlers/handlers/stats/fields/BuildBattleStats.java
@@ -3,13 +3,14 @@ package cc.hyperium.handlers.handlers.stats.fields;
 import cc.hyperium.handlers.handlers.stats.AbstractHypixelStats;
 
 public class BuildBattleStats extends AbstractHypixelStats {
-    @Override
-    public String getImage() {
-        return "BuildBattle-64";
-    }
 
-    @Override
-    public String getName() {
-        return "Build Battle";
-    }
+  @Override
+  public String getImage() {
+    return "BuildBattle-64";
+  }
+
+  @Override
+  public String getName() {
+    return "Build Battle";
+  }
 }
