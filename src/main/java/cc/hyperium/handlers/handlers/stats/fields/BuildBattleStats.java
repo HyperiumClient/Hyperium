b250b37b75bf8cef8302a9178b18f4eb7d0c9930
Koding King <djitechdude@gmail.com>
2018-08-24 19:13:15 +1000

    Finished Build Battle and ALL stats

--- src/main/java/cc/hyperium/handlers/handlers/stats/fields/BuildBattleStats.java
+++ src/main/java/cc/hyperium/handlers/handlers/stats/fields/BuildBattleStats.java
@@ -1,7 +1,17 @@
 package cc.hyperium.handlers.handlers.stats.fields;
 
 import cc.hyperium.handlers.handlers.stats.AbstractHypixelStats;
+import cc.hyperium.handlers.handlers.stats.display.DisplayLine;
+import cc.hyperium.handlers.handlers.stats.display.StatsDisplayItem;
+import cc.hyperium.utils.JsonHolder;
+import club.sk1er.website.api.requests.HypixelApiPlayer;
+import java.util.ArrayList;
+import java.util.List;
+import net.hypixel.api.GameType;
 
+/**
+ * @author KodingKing
+ */
 public class BuildBattleStats extends AbstractHypixelStats {
 
   @Override
@@ -12,5 +22,20 @@ public class BuildBattleStats extends AbstractHypixelStats {
   @Override
   public String getName() {
     return "Build Battle";
+  }
+
+  @Override
+  public List<StatsDisplayItem> getPreview(HypixelApiPlayer player) {
+    List<StatsDisplayItem> stats = new ArrayList<>();
+    JsonHolder arcade = player.getStats(GameType.ARCADE);
+
+    stats.add(new DisplayLine(bold("Build Battle wins: ", arcade.optInt("wins_buildbattle"))));
+
+    return stats;
+  }
+
+  @Override
+  public List<StatsDisplayItem> getDeepStats(HypixelApiPlayer player) {
+    return getPreview(player);
   }
 }
