80a48a0b93ffe3e154f979f457fa3f3794fd6a23
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-08-25 10:17:59 +0100

    remove * imports
    im on google settings now. it should stop doing this

--- src/main/java/cc/hyperium/config/Settings.java
+++ src/main/java/cc/hyperium/config/Settings.java
@@ -17,10 +17,20 @@
 
 package cc.hyperium.config;
 
+import static cc.hyperium.config.Category.ANIMATIONS;
+import static cc.hyperium.config.Category.CHROMAHUD;
+import static cc.hyperium.config.Category.COSMETICS;
+import static cc.hyperium.config.Category.GENERAL;
+import static cc.hyperium.config.Category.IMPROVEMENTS;
+import static cc.hyperium.config.Category.INTEGRATIONS;
+import static cc.hyperium.config.Category.MISC;
+import static cc.hyperium.config.Category.REACH;
+import static cc.hyperium.config.Category.SPOTIFY;
+import static cc.hyperium.config.Category.UTILITIES;
+import static cc.hyperium.config.Category.VANILLA_ENCHANTMENTS;
+
 import cc.hyperium.GuiStyle;
 import cc.hyperium.Hyperium;
-
-import static cc.hyperium.config.Category.*;
 
 /*
  * Created by Cubxity on 03/06/2018
