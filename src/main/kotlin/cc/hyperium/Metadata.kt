d82a9d8b1fc3c26bf1dbe00f0fcba739aa009202
Mitchell Katz <sk1er@lythrim.net>
2018-08-28 15:55:23 -0400

    Update version to 1.0

--- src/main/kotlin/cc/hyperium/Metadata.kt
+++ src/main/kotlin/cc/hyperium/Metadata.kt
@@ -30,7 +30,7 @@ val MODID
 
 val VERSION
     @JvmName("getVersion")
-    get() = "1.0 - Beta B24.1"
+    get() = "1.0"
 
 val AUTHORS
     @JvmName("getAuthors")
@@ -41,7 +41,7 @@ val AUTHORS
  */
 val VERSION_ID
     @JvmName("getVersionID")
-    get() = 36
+    get() = 37
 
 val ICON_BASE64
     @JvmName("getIconBase64")
