d78bc38a52c4a75dac8d4d0487ce8c3f919ccf40
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-09-05 19:48:49 +0100

    remove customrp from blacklisted
    now i can make and test updates yay

--- src/main/kotlin/cc/hyperium/Metadata.kt
+++ src/main/kotlin/cc/hyperium/Metadata.kt
@@ -62,4 +62,4 @@ val DEVELOPMENT
  */
 val BLACKLISTED
     @JvmName("getBlacklisted")
-    get() = arrayOf("AutoFriend", "Custom Crosshair Addon", "CustomRP addon", "PlayTime addon","Tab Toggle")
+    get() = arrayOf("AutoFriend", "Custom Crosshair Addon", "PlayTime addon", "Tab Toggle")
