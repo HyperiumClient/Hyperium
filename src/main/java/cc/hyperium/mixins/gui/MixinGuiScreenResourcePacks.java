9c64da16c679eb88ae9830490e231a67af1ee002
CoalCoding <coalores@gmail.com>
2018-09-07 16:58:29 +0100

    Fixed and tweaked resource pack GUI.

--- src/main/java/cc/hyperium/mixins/gui/MixinGuiScreenResourcePacks.java
+++ src/main/java/cc/hyperium/mixins/gui/MixinGuiScreenResourcePacks.java
@@ -1,19 +1,11 @@
 package cc.hyperium.mixins.gui;
 
 import cc.hyperium.mixinsimp.gui.HyperiumGuiScreenResourcePacks;
-import cc.hyperium.utils.ChatColor;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
-import me.semx11.autotip.util.ReflectionUtil;
 import net.minecraft.client.gui.GuiResourcePackAvailable;
 import net.minecraft.client.gui.GuiResourcePackSelected;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
 import net.minecraft.client.gui.GuiTextField;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.resources.ResourcePackListEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -22,113 +14,71 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.IOException;
+import java.util.List;
+
 /**
  * @author KodingKing
  */
 @Mixin(GuiScreenResourcePacks.class)
 public class MixinGuiScreenResourcePacks extends GuiScreen {
 
-  @Shadow
-  private GuiResourcePackAvailable availableResourcePacksList;
-  @Shadow
-  private GuiResourcePackSelected selectedResourcePacksList;
-  @Shadow
-  private List<ResourcePackListEntry> availableResourcePacks;
-  private HyperiumGuiScreenResourcePacks hyperiumGuiResourcePack = new HyperiumGuiScreenResourcePacks(
-      (GuiScreenResourcePacks) (Object) this);
+    @Shadow
+    private GuiResourcePackAvailable availableResourcePacksList;
+    @Shadow
+    private GuiResourcePackSelected selectedResourcePacksList;
+    @Shadow
+    private List<ResourcePackListEntry> availableResourcePacks;
+    private HyperiumGuiScreenResourcePacks hyperiumGuiResourcePack = new HyperiumGuiScreenResourcePacks(
+            (GuiScreenResourcePacks) (Object) this);
 
-  private GuiResourcePackAvailable availablePacksClone;
+    private GuiResourcePackAvailable availablePacksClone;
 
-  private GuiTextField searchField;
+    private GuiTextField searchField;
 
-  /**
-   * @reason Change buttons size
-   * @author SiroQ
-   */
-  @Inject(method = "initGui", at = @At("RETURN"))
-  public void initGui(CallbackInfo callbackInfo) {
-    hyperiumGuiResourcePack.initGui(this.buttonList);
+    /**
+     * @reason Change buttons size
+     * @author SiroQ
+     */
+    @Inject(method = "initGui", at = @At("RETURN"))
+    public void initGui(CallbackInfo callbackInfo) {
+        hyperiumGuiResourcePack.initGui(this.buttonList);
 
-    this.availablePacksClone = this.availableResourcePacksList;
-    this.searchField = new GuiTextField(3, fontRendererObj, this.width / 2 - 4 - 200,
-        this.height - 24, 200, 20);
-  }
-
-  @Override
-  protected void keyTyped(char typedChar, int keyCode) throws IOException {
-    super.keyTyped(typedChar, keyCode);
-
-    if (this.searchField != null) {
-      this.searchField.textboxKeyTyped(typedChar, keyCode);
+        this.availablePacksClone = this.availableResourcePacksList;
+        this.searchField = new GuiTextField(3, fontRendererObj, this.width / 2 - 4 - 200,
+                this.height - 24, 200, 20);
     }
 
-    if (this.searchField == null || this.searchField.getText().isEmpty()) {
-      this.availableResourcePacksList = new GuiResourcePackAvailable(this.mc, 200, this.height,
-          this.availableResourcePacks);
-      this.availableResourcePacksList.setSlotXBoundsFromLeft(this.width / 2 - 4 - 200);
-      this.availableResourcePacksList.registerScrollButtons(7, 8);
-    } else {
-      this.availableResourcePacksList = new GuiResourcePackAvailable(this.mc, 200, this.height,
-          Arrays
-              .asList(this.availablePacksClone.getList().stream().filter(resourcePackListEntry -> {
-                try {
-                  Method nameMethod = ReflectionUtil
-                      .findDeclaredMethod(resourcePackListEntry.getClass(),
-                          new String[]{"func_148312_b", "c"});
-                  nameMethod.setAccessible(true);
-                  String name = ChatColor
-                      .stripColor((String) nameMethod.invoke(resourcePackListEntry))
-                      .replaceAll("[^A-Za-z0-9 ]", "").trim().toLowerCase();
-                  String text = searchField.getText().toLowerCase();
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        super.keyTyped(typedChar, keyCode);
 
-                  if (name.endsWith("zip")) {
-                    name = name.subSequence(0, name.length() - 3).toString();
-                  }
+        if (this.searchField != null) {
+            this.searchField.textboxKeyTyped(typedChar, keyCode);
+        }
 
-                  for (String s : text.split(" ")) {
-                    if (!name.contains(s.toLowerCase())) {
-                      return false;
-                    }
-                  }
-
-                  return name.startsWith(text) || name.contains(text) || name
-                      .equalsIgnoreCase(text);
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                  e.printStackTrace();
-                  return true;
-                }
-              }).toArray(ResourcePackListEntry[]::new)));
-      this.availableResourcePacksList.setSlotXBoundsFromLeft(this.width / 2 - 4 - 200);
-      this.availableResourcePacksList.registerScrollButtons(7, 8);
+        updateList();
     }
-  }
 
-  @Override
-  protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-    super.mouseClicked(mouseX, mouseY, mouseButton);
-
-    if (this.searchField != null) {
-      this.searchField.mouseClicked(mouseX, mouseY, mouseButton);
+    @Inject(method = "mouseClicked", at = @At("RETURN"))
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton, CallbackInfo ci) throws IOException {
+        if (this.searchField != null) {
+            this.searchField.mouseClicked(mouseX, mouseY, mouseButton);
+        }
+        updateList();
     }
-  }
 
-  /**
-   * @reason Change text location
-   * @author SiroQ
-   */
-  @Overwrite
-  public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-    this.drawBackground(0);
-    this.availableResourcePacksList.drawScreen(mouseX, mouseY, partialTicks);
-    this.selectedResourcePacksList.drawScreen(mouseX, mouseY, partialTicks);
-    this.drawCenteredString(this.fontRendererObj, I18n.format("resourcePack.title"), this.width / 2,
-        16, 16777215);
-    this.drawCenteredString(this.fontRendererObj, I18n.format("resourcePack.folderInfo"),
-        this.width / 2 - 102, this.height - 26, 8421504);
+    /**
+     * @reason Change text location
+     * @author SiroQ
+     */
+    @Overwrite
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        hyperiumGuiResourcePack.drawScreen(availableResourcePacksList,selectedResourcePacksList,mouseX,mouseY,partialTicks,fontRendererObj,searchField,width);
+        super.drawScreen(mouseX,mouseY,partialTicks);
+    }
 
-    this.searchField.drawTextBox();
-
-    super.drawScreen(mouseX, mouseY, partialTicks);
-  }
-
+    public void updateList(){
+        availableResourcePacksList = hyperiumGuiResourcePack.updateList(searchField,availablePacksClone,availableResourcePacks,mc,height,width);
+    }
 }
