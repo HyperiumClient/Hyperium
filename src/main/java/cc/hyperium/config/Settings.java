b676bce60f91023a5617bd2cae794c2160335df2
SiroQ <siro0644@gmail.com>
2018-09-12 15:54:28 +0900

    Add option to use the 1.7 debug (F3) menu (#443)
    * Add old debug menu
    
    * Fix few vars
    
    * Code fix

--- src/main/java/cc/hyperium/config/Settings.java
+++ src/main/java/cc/hyperium/config/Settings.java
@@ -214,6 +214,10 @@ public class Settings {
     public static boolean OLD_ITEM_HELD = false;
 
     @ConfigOpt
+    @ToggleSetting(name = "1.7 Debug", category = ANIMATIONS, mods = true)
+    public static boolean OLD_DEBUG = false;
+
+    @ConfigOpt
     @ToggleSetting(name = "Custom Sword Animation", category = ANIMATIONS, mods = true)
     public static boolean CUSTOM_SWORD_ANIMATION = false;
 
