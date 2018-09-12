ed0349ef5c21a0b072d624af034c11c3f9d9801f
SiroQ <siro0644@gmail.com>
2018-09-12 20:56:26 +0900

    Add BossbarAddon

---
+++ src/main/java/cc/hyperium/addons/bossbar/gui/GuiBossbarPosition.java
@@ -0,0 +1,65 @@
+package cc.hyperium.addons.bossbar.gui;
+
+import cc.hyperium.addons.bossbar.BossbarAddon;
+import cc.hyperium.addons.bossbar.config.BossbarConfig;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.boss.BossStatus;
+
+import java.io.IOException;
+
+public class GuiBossbarPosition extends GuiScreen {
+    private GuiScreen parent;
+    private BossbarAddon addon;
+    private String oldBossName;
+    private int oldStatusBarTime;
+    private float oldHealthScale;
+    public GuiBossbarPosition(GuiScreen parent, BossbarAddon addon) {
+        this.parent = parent;
+        this.addon = addon;
+    }
+
+    @Override
+    public void onGuiClosed() {
+        BossStatus.bossName = oldBossName;
+        BossStatus.statusBarTime = oldStatusBarTime;
+        BossStatus.healthScale = oldHealthScale;
+        super.onGuiClosed();
+    }
+
+    @Override
+    public void initGui() {
+        oldBossName = BossStatus.bossName;
+        BossStatus.bossName = "Sample";
+        oldStatusBarTime = BossStatus.statusBarTime;
+        BossStatus.statusBarTime = Integer.MAX_VALUE;
+        oldHealthScale = BossStatus.healthScale;
+        BossStatus.healthScale = 0.5F;
+        this.buttonList.add(new GuiButton(0, this.getCenter() - 100, this.getRowPos(), "Done"));
+    }
+
+    @Override
+    protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
+        BossbarConfig.x = mouseX;
+        BossbarConfig.y = mouseY;
+        super.mouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick);
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        switch (button.id){
+            case 0:
+                mc.displayGuiScreen(parent);
+                break;
+        }
+        super.actionPerformed(button);
+    }
+
+    public int getRowPos() {
+        return this.height - 36;
+    }
+
+    public int getCenter() {
+        return this.width / 2;
+    }
+}
---
+++ src/main/java/cc/hyperium/addons/bossbar/gui/GuiBossbarSetting.java
@@ -0,0 +1,75 @@
+package cc.hyperium.addons.bossbar.gui;
+
+import cc.hyperium.addons.bossbar.BossbarAddon;
+import cc.hyperium.addons.bossbar.config.BossbarConfig;
+import cc.hyperium.addons.sidebar.gui.screen.GuiScreenBackground;
+import cc.hyperium.config.ConfigOpt;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.fml.client.config.GuiSlider;
+
+import java.io.IOException;
+
+public class GuiBossbarSetting extends GuiScreen {
+    private BossbarAddon addon;
+    private GuiSlider sliderScale;
+
+    public GuiBossbarSetting(BossbarAddon addon) {
+        this.addon = addon;
+    }
+
+    @Override
+    public void initGui() {
+        this.buttonList.add(new GuiButton(0, this.getCenter() - 75, this.getRowPos(1), 150, 20, "Bossbar: " + this.getSuffix(BossbarConfig.bossBarEnabled)));
+        this.buttonList.add(new GuiButton(1, this.getCenter() - 75, this.getRowPos(2), 150, 20, "Text: " + this.getSuffix(BossbarConfig.textEnabled)));
+        this.buttonList.add(new GuiButton(2, this.getCenter() - 75, this.getRowPos(3), 150, 20, "Bar: " + this.getSuffix(BossbarConfig.barEnabled)));
+        this.buttonList.add(new GuiButton(3, this.getCenter() - 75, this.getRowPos(4), 150, 20, "Set Position"));
+        this.buttonList.add(new GuiButton(4, this.getCenter() - 75, this.getRowPos(5), 150, 20, "Reset Bossbar"));
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        switch (button.id){
+            case 0:
+                BossbarConfig.bossBarEnabled = !BossbarConfig.bossBarEnabled;
+                button.displayString = "Bossbar: " + this.getSuffix(BossbarConfig.bossBarEnabled);
+                break;
+            case 1:
+                BossbarConfig.textEnabled = !BossbarConfig.textEnabled;
+                button.displayString = "Text: " + this.getSuffix(BossbarConfig.textEnabled);
+                break;
+            case 2:
+                BossbarConfig.barEnabled = !BossbarConfig.barEnabled;
+                button.displayString = "Bar: " + this.getSuffix(BossbarConfig.barEnabled);
+                break;
+            case 3:
+                mc.displayGuiScreen(new GuiBossbarPosition(this, this.addon));
+                break;
+            case 4:
+                BossbarConfig.bossBarEnabled = true;
+                BossbarConfig.barEnabled = true;
+                BossbarConfig.textEnabled = true;
+                BossbarConfig.x = -1;
+                BossbarConfig.y = 12;
+        }
+        super.actionPerformed(button);
+    }
+    public int getRowPos(final int rowNumber) {
+        return this.height / 4 + (24 * rowNumber - 24) - 16;
+    }
+
+    public int getCenter() {
+        return this.width / 2;
+    }
+
+    private String getSuffix(final boolean enabled) {
+        return enabled ? (EnumChatFormatting.GREEN + "Enabled") : (EnumChatFormatting.RED + "Disabled");
+    }
+}
