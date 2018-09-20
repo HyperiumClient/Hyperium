d0c28ea326476e4ba7b6507fd98eaf77f1690257
boomboompower <boomboompowermc@gmail.com>
2018-09-20 16:48:42 +1000

    Clean SplashProgress

--- src/main/java/cc/hyperium/SplashProgress.java
+++ src/main/java/cc/hyperium/SplashProgress.java
@@ -18,6 +18,7 @@
 package cc.hyperium;
 
 import cc.hyperium.utils.HyperiumFontRenderer;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
@@ -25,13 +26,12 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.util.ResourceLocation;
-import org.bridj.Pointer;
-import org.bridj.cpp.com.shell.ITaskbarList3;
 
 import java.awt.Color;
 import java.awt.Font;
 
 public class SplashProgress {
+
     public static final int DEFAULT_MAX = 13;
     public static int MAX = DEFAULT_MAX;
     public static int PROGRESS = 0;
@@ -39,43 +39,24 @@ public class SplashProgress {
     private static ResourceLocation splash;
     private static TextureManager ctm;
     public static boolean CANCEL_IF_MAX = true;
+
     /**
      * FontRenderer for drawing splash screen
      */
     private static HyperiumFontRenderer sfr;
 
-    private static ITaskbarList3 taskbar;
-    private static Pointer<?> hwnd;
-    private static boolean once = false;
-
     public static void update() {
-        if (hwnd != null && taskbar != null)
-            try {
-                System.out.println("DEBUG: Setting progress value: " + PROGRESS + "/" + MAX);
-                taskbar.SetProgressValue((Pointer<Integer>) hwnd, PROGRESS, MAX);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        if (PROGRESS == MAX && CANCEL_IF_MAX)
-            finish();
-        if (Minecraft.getMinecraft() == null || Minecraft.getMinecraft().getLanguageManager() == null)
+        if (Minecraft.getMinecraft() == null || Minecraft.getMinecraft().getLanguageManager() == null) {
             return;
+        }
         drawSplash(Minecraft.getMinecraft().getTextureManager());
     }
 
-    public static void finish() {
-        if (taskbar != null)
-            try {
-                taskbar.SetProgressState((Pointer<Integer>) hwnd, ITaskbarList3.TbpFlag.TBPF_NOPROGRESS);
-                taskbar.Release();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-    }
-
     public static void drawSplash(TextureManager tm) {
-        if (ctm == null)
+        if (ctm == null) {
             ctm = tm;
+        }
+
         ScaledResolution scaledresolution = new ScaledResolution(Minecraft.getMinecraft());
         int i = scaledresolution.getScaleFactor();
         Framebuffer framebuffer = new Framebuffer(scaledresolution.getScaledWidth() * i, scaledresolution.getScaledHeight() * i, true);
@@ -90,9 +71,13 @@ public class SplashProgress {
         GlStateManager.disableFog();
         GlStateManager.disableDepth();
         GlStateManager.enableTexture2D();
-        if (splash == null)
+
+        if (splash == null) {
             splash = new ResourceLocation("textures/hyperium-splash.png");
+        }
+
         tm.bindTexture(splash);
+
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         Gui.drawScaledCustomSizeModalRect(0, 0, 0, 0, 1920, 1080, scaledresolution.getScaledWidth(), scaledresolution.getScaledHeight(), 1920, 1080);
         drawProgress();
@@ -101,33 +86,23 @@ public class SplashProgress {
         GlStateManager.enableAlpha();
         GlStateManager.alphaFunc(516, 0.1F);
         Minecraft.getMinecraft().updateDisplay();
-
-        if (once)
-            return;
-//        try {
-//            once = true;
-//            taskbar = COMRuntime.newInstance(ITaskbarList3.class);
-//            Method m = Display.class.getDeclaredMethod("getImplementation");
-//            m.setAccessible(true);
-//            Object displayImpl = m.invoke(null);
-//            Field f = displayImpl.getClass().getDeclaredField("hwnd");
-//            f.setAccessible(true);
-//            hwnd = Pointer.pointerToAddress((Long) f.get(displayImpl));
-//            taskbar.SetProgressValue((Pointer<Integer>) hwnd, 1, MAX);
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
     }
 
     private static void drawProgress() {
-        if (Minecraft.getMinecraft().gameSettings == null || Minecraft.getMinecraft().getTextureManager() == null)
+        if (Minecraft.getMinecraft().gameSettings == null || Minecraft.getMinecraft().getTextureManager() == null) {
             return;
-        if (sfr == null)
+        }
+
+        if (sfr == null) {
             sfr = new HyperiumFontRenderer("Arial", Font.PLAIN, 20);
+        }
+
         ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
+
         double nProgress = (double) PROGRESS;
         double calc = (nProgress / MAX) * 150;
-        sfr.drawString(CURRENT, sr.getScaledWidth() / 2 - sfr.getWidth(CURRENT) / 2, sr.getScaledHeight() / 2 + 60, new Color(0, 150, 250).getRGB());
+
+        sfr.drawString(CURRENT, (float) sr.getScaledWidth_double() / 2 - sfr.getWidth(CURRENT) / 2, (float) sr.getScaledHeight_double() / 2 + 60, new Color(0, 150, 250).getRGB());
         // Progress
         Gui.drawRect(sr.getScaledWidth() / 2 - 75, sr.getScaledHeight() / 2 + 45, sr.getScaledWidth() / 2 + (int) (Math.round(calc) / 2), sr.getScaledHeight() / 2 + 55, new Color(255, 0, 0, 50).getRGB());
         // Bar base
