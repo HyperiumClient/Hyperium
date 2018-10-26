dc463381a73d574862182283471d4b250c55e056
KodingKing <KodingKing1@users.noreply.github.com>
2018-10-27 03:14:35 +1100

    Fix browser crash (#515)
    * If this still crashes there is no hope
    
    * Fixed Travis
    
    * Fixed Travis... again

--- src/main/java/cc/hyperium/mods/browser/gui/GuiBrowser.java
+++ src/main/java/cc/hyperium/mods/browser/gui/GuiBrowser.java
@@ -23,6 +23,7 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.Map;
+import java.lang.Exception;
 
 /**
  * @author Koding
@@ -289,6 +290,7 @@ public class GuiBrowser extends GuiScreen {
     //Handle button clicks
     @Override
     protected void actionPerformed(GuiButton src) {
+        try {
         if (browser == null || src == null) {
             return;
         }
@@ -315,6 +317,9 @@ public class GuiBrowser extends GuiScreen {
             GuiConfig.drawSquare = true;
             mc.displayGuiScreen(persistentConfigGui);
 
+        }
+        } catch (Exception e) {
+            
         }
     }
 
