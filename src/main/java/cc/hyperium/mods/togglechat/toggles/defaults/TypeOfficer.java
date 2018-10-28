ad1cbdf116749b1979b3695a6e4ac726509077a1
KodingKing <KodingKing1@users.noreply.github.com>
2018-10-28 11:44:15 +1100

    Update TypeOfficer.java

--- src/main/java/cc/hyperium/mods/togglechat/toggles/defaults/TypeOfficer.java
+++ src/main/java/cc/hyperium/mods/togglechat/toggles/defaults/TypeOfficer.java
@@ -6,10 +6,8 @@ import java.util.regex.Pattern;
 
 public class TypeOfficer extends ToggleBase {
 
-    private final Pattern longPattern = Pattern.compile(
-        "Officer > (?<rank>\\[.+] )?(?<player>\\S{1,16})?(?<role>\\[.+])*: (?<message>.*)");
-    private final Pattern shortPattern = Pattern
-        .compile("O > (?<rank>\\[.+] )?(?<player>\\S{1,16})?(?<role>\\[.+])*: (?<message>.*)");
+    private final Pattern pattern = Pattern.compile(
+        "(O|Officer) > (?<rank>\\[.+] )?(?<player>\\S{1,16})?(?<role>\\[.+])*: (?<message>.*)");
 
     private boolean enabled = true;
 
@@ -20,8 +18,7 @@ public class TypeOfficer extends ToggleBase {
 
     @Override
     public boolean shouldToggle(String message) {
-        return this.longPattern.matcher(message).find() ||
-            this.shortPattern.matcher(message).find();
+        return this.pattern.matcher(message).find();
     }
 
     @Override
