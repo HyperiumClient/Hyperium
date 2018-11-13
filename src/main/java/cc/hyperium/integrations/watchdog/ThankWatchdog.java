22fde5644bf25938a23e9b4255f64bc4fde812b6
ChachyGit <38067923+ChachyGit@users.noreply.github.com>
2018-11-13 16:45:10 +0000

    final changes :joy:

--- src/main/java/cc/hyperium/integrations/watchdog/ThankWatchdog.java
+++ src/main/java/cc/hyperium/integrations/watchdog/ThankWatchdog.java
@@ -7,11 +7,11 @@ import cc.hyperium.event.InvokeEvent;
 import net.minecraft.client.Minecraft;
 
 public class ThankWatchdog {
+  private static final String watchdogBanMessage = "A player has been removed from your game for hacking or abuse. Thanks for reporting it!";
+  private static final String thankWatchDogMessage = "Thanks Watchdog!";
 
   @InvokeEvent
   public void onChat(ChatEvent e) {
-    final String watchdogBanMessage = "A player has been removed from your game for hacking or abuse. Thanks for reporting it!";
-    String thankWatchDogMessage = "Thanks Watchdog!";
     if (e.getChat().getUnformattedText().contains(watchdogBanMessage) && Settings.THANK_WATCHDOG) {
       Minecraft.getMinecraft().thePlayer.sendChatMessage(thankWatchDogMessage);
     }
