a484e54f608edb563a9cfecc159c1b7692c883c1
RDIL <34555510+RDIL@users.noreply.github.com>
2018-11-15 22:34:57 -0500

    o no hyperium is already taken guess were using hyperiumclient

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Hyperium'
-copyright = '2018 - Present Hyperium Dev Team / Sk1er LLC.'
+project = 'HyperiumClient'
+copyright = '2018 - Present, Hyperium Dev Team / Sk1er LLC.'
 author = 'Sk1er'
 
 # The short X.Y version
@@ -102,7 +102,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Hyperiumdoc'
+htmlhelp_basename = 'HyperiumClientdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -129,7 +129,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Hyperium.tex', 'Hyperium Documentation',
+    (master_doc, 'HyperiumClient.tex', 'HyperiumClient Documentation',
      'Sk1er', 'manual'),
 ]
 
@@ -139,7 +139,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'hyperium', 'Hyperium Documentation',
+    (master_doc, 'hyperiumclient', 'HyperiumClient Documentation',
      [author], 1)
 ]
 
@@ -150,8 +150,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Hyperium', 'Hyperium Documentation',
-     author, 'Hyperium', 'One line description of project.',
+    (master_doc, 'HyperiumClient', 'HyperiumClient Documentation',
+     author, 'HyperiumClient', 'One line description of project.',
      'Miscellaneous'),
 ]
 
