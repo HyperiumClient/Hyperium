f74beb28c85a355c606fb773114f94a5100ea88c
RDIL <34555510+RDIL@users.noreply.github.com>
2018-11-19 16:45:25 -0500

    fix double import

--- src/main/java/cc/hyperium/mixins/packet/MixinNetHandlerPlayClient.java
+++ src/main/java/cc/hyperium/mixins/packet/MixinNetHandlerPlayClient.java
@@ -58,7 +58,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
