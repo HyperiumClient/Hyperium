b4175e1b3e14a6a021f3057140984125a6f000b5
RDIL <34555510+RDIL@users.noreply.github.com>
2018-11-29 13:15:58 -0500

    k

--- src/main/java/cc/hyperium/integrations/BetterChatFilter/BetterChatFilter.java
+++ src/main/java/cc/hyperium/integrations/BetterChatFilter/BetterChatFilter.java
@@ -60,11 +60,8 @@ public class BetterChatFilter {
     try {
       final String rawBadwords = IOUtils.toString(new URL(badWordsURL), Charset.forName("UTF-8"));
       badWords = new ArrayList<>(Arrays.asList(rawBadwords.split("\n")));
-      return true;
     } catch (Exception e) {
-      System.out.println("[BetterChatFilter] Failed to download bad word file");
       e.printStackTrace();
-      return false;
     }
   }
   
