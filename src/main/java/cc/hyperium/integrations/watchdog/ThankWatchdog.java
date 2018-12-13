61c37d98aa41df9d4565bd4cfbf52f264679351b
asbyth <36578995+asbyth@users.noreply.github.com>
2018-12-13 10:08:49 -0500

    achat

--- src/main/java/cc/hyperium/integrations/watchdog/ThankWatchdog.java
+++ src/main/java/cc/hyperium/integrations/watchdog/ThankWatchdog.java
@@ -9,12 +9,12 @@ import net.minecraft.client.Minecraft;
 public class ThankWatchdog {
   private static final String WATCHDOG_BAN_TRIGGER = "A player has been removed from your game for hacking or abuse. Thanks for reporting it!";
   private static final String WATCHDOG_ANNOUNCEMENT_TRIGGER = "[WATCHDOG ANNOUNCEMENT]";
-  private static final String THANK_WATCHDOG_MESSAGE = "Thanks Watchdog!";
+  private static final String THANK_WATCHDOG_MESSAGE = "/achat Thanks Watchdog!";
 
   @InvokeEvent
   public void onChat(ChatEvent e) {
     if (e.getChat().getUnformattedText().contains(WATCHDOG_BAN_TRIGGER) || e.getChat().getUnformattedText().contains(WATCHDOG_ANNOUNCEMENT_TRIGGER)) {
-      if(Settings.THANK_WATCHDOG) { Minecraft.getMinecraft().thePlayer.sendChatMessage("/achat " + THANK_WATCHDOG_MESSAGE); }
+      if(Settings.THANK_WATCHDOG) { Minecraft.getMinecraft().thePlayer.sendChatMessage(THANK_WATCHDOG_MESSAGE); }
     }
   }
 }
