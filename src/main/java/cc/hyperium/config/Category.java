04e70d41f26f9b2ff933490d9395f9607ad7b818
Reece Dunham <me@rdil.rocks>
2018-12-31 14:55:47 -0700

    DONE!!

--- src/main/java/cc/hyperium/config/Category.java
+++ src/main/java/cc/hyperium/config/Category.java
@@ -30,7 +30,9 @@ public enum Category {
     TAB_TOGGLE("Tab Toggle"),
     ITEM_PHYSIC("Item Physics"),
     VICTORYROYALE("Victory Royale"),
-    TOGGLESPRINT("Toggle Sprint");
+    TOGGLESPRINT("Toggle Sprint"),
+    CHUNK_ANIMATOR("Chunk Animator");
+    
     private String display;
 
     Category(String display) {
