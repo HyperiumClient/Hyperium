22d8a9ade5f30f0f923241d2088d6ab23062fe27
Reece Dunham <me@rdil.rocks>
2019-02-03 13:32:30 -0500

    Address feedback

--- src/main/java/cc/hyperium/mods/victoryroyale/VictoryRoyale.java
+++ src/main/java/cc/hyperium/mods/victoryroyale/VictoryRoyale.java
@@ -18,7 +18,6 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
-import org.jetbrains.annotations.Contract;
 import org.lwjgl.opengl.GL11;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -32,7 +31,6 @@ public class VictoryRoyale extends AbstractMod {
 
     private long start = 0;
 
-    @Contract(pure = true)
     public static VictoryRoyale getInstance() {
         return INSTANCE;
     }
@@ -154,7 +152,6 @@ public class VictoryRoyale extends AbstractMod {
             GlStateManager.translate(imageWidth, 10, 0);
 
             Gui.drawScaledCustomSizeModalRect(0, 0, 0, 0, 1200, 675, imageWidth * 2, imageWidth, 1200, 675);
-            System.out.println("victory royale (debug)");
             GlStateManager.popMatrix();
         }
     }
@@ -202,7 +199,7 @@ public class VictoryRoyale extends AbstractMod {
         private double xPercent, yPercent;
         private int width;
 
-        protected WhiteLine(double xPercent, double yPercent, int width) {
+        public WhiteLine(double xPercent, double yPercent, int width) {
             this.xPercent = xPercent;
             this.yPercent = yPercent;
             this.width = width;
