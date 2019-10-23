9f822ba863debe7a457df841f5e6dac8ff04e8b2
asbyth <cyronize@gmail.com>
2019-10-23 14:24:15 -0400

    fix up a few togglechat messages

--- src/main/java/cc/hyperium/mods/togglechat/gui/ToggleChatMainGui.java
+++ src/main/java/cc/hyperium/mods/togglechat/gui/ToggleChatMainGui.java
@@ -82,8 +82,8 @@ public class ToggleChatMainGui extends GuiScreen {
                 position[0] += 24;
             });
 
-            GuiButton last = new GuiButton(1, this.width / 2 - 51, this.height / 2 + 90, 50, 20, "\u21E6");
-            GuiButton next = new GuiButton(2, this.width / 2 + 1, this.height / 2 + 90, 50, 20, "\u21E8");
+            GuiButton last = new GuiButton(1, this.width / 2 - 51, this.height / 2 + 90, 50, 20, "<");
+            GuiButton next = new GuiButton(2, this.width / 2 + 1, this.height / 2 + 90, 50, 20, ">");
 
             this.buttonList.add(last);
             this.buttonList.add(next);
