c590f6261794566400d48840febb1ff51025103c
asbyth <cyronize@gmail.com>
2019-10-24 11:01:46 -0400

    scrollable tooltips

--- src/main/java/cc/hyperium/mixins/client/gui/MixinGuiScreen.java
+++ src/main/java/cc/hyperium/mixins/client/gui/MixinGuiScreen.java
@@ -22,10 +22,15 @@ import cc.hyperium.config.Settings;
 import cc.hyperium.mixinsimp.client.gui.HyperiumGuiScreen;
 import com.chattriggers.ctjs.triggers.TriggerType;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.util.IChatComponent;
+import net.minecraftforge.fml.client.config.GuiUtils;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -35,6 +40,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(GuiScreen.class)
 public abstract class MixinGuiScreen {
@@ -44,6 +51,12 @@ public abstract class MixinGuiScreen {
     @Shadow protected abstract void actionPerformed(GuiButton button) throws IOException;
     @Shadow protected abstract void keyTyped(char typedChar, int keyCode) throws IOException;
 
+    @Shadow
+    protected FontRenderer fontRendererObj;
+    @Shadow
+    public int width;
+    @Shadow
+    public int height;
     private HyperiumGuiScreen hyperiumGuiScreen = new HyperiumGuiScreen((GuiScreen) (Object) this);
 
     /**
@@ -102,5 +115,177 @@ public abstract class MixinGuiScreen {
     )
     private void runCommand(IChatComponent chatComponent, CallbackInfoReturnable<Boolean> cir) {
         Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().runningCommand = true;
+    }
+
+    private int scrollX, scrollY;
+    private boolean allowScrolling;
+
+    /**
+     * @author asbyth
+     * @reason Scrollable Tooltips
+     */
+    @Overwrite
+    protected void drawHoveringText(List<String> textLines, int x, int y) {
+        drawScrollableHoveringText(textLines, x, y, width, height, -1, fontRendererObj);
+    }
+
+    /**
+     * Draws a tooltip box on the screen with text in it.
+     * Automatically positions the box relative to the mouse to match Mojang's implementation.
+     * Automatically wraps text when there is not enough space on the screen to display the text without wrapping.
+     * Can have a maximum width set to avoid creating very wide tooltips.
+     *
+     * @param textLines    the lines of text to be drawn in a hovering tooltip box.
+     * @param mouseX       the mouse X position
+     * @param mouseY       the mouse Y position
+     * @param screenWidth  the available screen width for the tooltip to drawn in
+     * @param screenHeight the available  screen height for the tooltip to drawn in
+     * @param maxTextWidth the maximum width of the text in the tooltip box.
+     * @param font         the font for drawing the text in the tooltip box
+     */
+    private void drawScrollableHoveringText(List<String> textLines, int mouseX, int mouseY, int screenWidth, int screenHeight, int maxTextWidth, FontRenderer font) {
+        if (!textLines.isEmpty()) {
+            if (!allowScrolling) {
+                scrollX = 0;
+                scrollY = 0;
+            }
+
+            GlStateManager.disableRescaleNormal();
+            RenderHelper.disableStandardItemLighting();
+            GlStateManager.disableLighting();
+            GlStateManager.disableDepth();
+            int tooltipTextWidth = 0;
+
+            for (String textLine : textLines) {
+                int textLineWidth = font.getStringWidth(textLine);
+
+                if (textLineWidth > tooltipTextWidth) {
+                    tooltipTextWidth = textLineWidth;
+                }
+            }
+
+            boolean needsWrap = false;
+
+            int titleLinesCount = 1;
+            int tooltipX = mouseX + 12;
+            if (tooltipX + tooltipTextWidth + 4 > screenWidth) {
+                tooltipX = mouseX - 16 - tooltipTextWidth;
+                if (tooltipX < 4) { // if the tooltip doesn't fit on the screen
+                    if (mouseX > screenWidth / 2) {
+                        tooltipTextWidth = mouseX - 12 - 8;
+                    } else {
+                        tooltipTextWidth = screenWidth - 16 - mouseX;
+                    }
+                    needsWrap = true;
+                }
+            }
+
+            if (maxTextWidth > 0 && tooltipTextWidth > maxTextWidth) {
+                tooltipTextWidth = maxTextWidth;
+                needsWrap = true;
+            }
+
+            if (needsWrap) {
+                int wrappedTooltipWidth = 0;
+                List<String> wrappedTextLines = new ArrayList<>();
+                for (int i = 0; i < textLines.size(); i++) {
+                    String textLine = textLines.get(i);
+                    List<String> wrappedLine = font.listFormattedStringToWidth(textLine, tooltipTextWidth);
+                    if (i == 0) {
+                        titleLinesCount = wrappedLine.size();
+                    }
+
+                    for (String line : wrappedLine) {
+                        int lineWidth = font.getStringWidth(line);
+                        if (lineWidth > wrappedTooltipWidth) {
+                            wrappedTooltipWidth = lineWidth;
+                        }
+                        wrappedTextLines.add(line);
+                    }
+                }
+
+                tooltipTextWidth = wrappedTooltipWidth;
+                textLines = wrappedTextLines;
+
+                if (mouseX > screenWidth / 2) {
+                    tooltipX = mouseX - 16 - tooltipTextWidth;
+                } else {
+                    tooltipX = mouseX + 12;
+                }
+            }
+
+            int tooltipY = mouseY - 12;
+            int tooltipHeight = 8;
+
+            if (textLines.size() > 1) {
+                tooltipHeight += (textLines.size() - 1) * 10;
+                if (textLines.size() > titleLinesCount) {
+                    tooltipHeight += 2; // gap between title lines and next lines
+                }
+            }
+
+            if (tooltipY + tooltipHeight + 6 > screenHeight) {
+                tooltipY = screenHeight - tooltipHeight - 6;
+            }
+
+            allowScrolling = tooltipY < 0;
+            GlStateManager.pushMatrix();
+            if (allowScrolling) {
+                int eventDWheel = Mouse.getDWheel();
+                if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+                    if (eventDWheel < 0) {
+                        scrollX += 10;
+                    } else if (eventDWheel > 0) {
+                        //Scrolling to access higher stuff
+                        scrollX -= 10;
+                    }
+                } else {
+                    if (eventDWheel < 0) {
+                        scrollY -= 10;
+                    } else if (eventDWheel > 0) {
+                        //Scrolling to access higher stuff
+                        scrollY += 10;
+                    }
+                }
+
+                if (scrollY + tooltipY > 6) {
+                    scrollY = -tooltipY + 6;
+                } else if (scrollY + tooltipY + tooltipHeight + 6 < screenHeight) {
+                    scrollY = screenHeight - 6 - tooltipY - tooltipHeight;
+                }
+            }
+
+            GlStateManager.translate(scrollX, scrollY, 0);
+            int zLevel = 300;
+            int backgroundColor = 0xF0100010;
+            GuiUtils.drawGradientRect(zLevel, tooltipX - 3, tooltipY - 4, tooltipX + tooltipTextWidth + 3, tooltipY - 3, backgroundColor, backgroundColor);
+            GuiUtils.drawGradientRect(zLevel, tooltipX - 3, tooltipY + tooltipHeight + 3, tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 4, backgroundColor, backgroundColor);
+            GuiUtils.drawGradientRect(zLevel, tooltipX - 3, tooltipY - 3, tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 3, backgroundColor, backgroundColor);
+            GuiUtils.drawGradientRect(zLevel, tooltipX - 4, tooltipY - 3, tooltipX - 3, tooltipY + tooltipHeight + 3, backgroundColor, backgroundColor);
+            GuiUtils.drawGradientRect(zLevel, tooltipX + tooltipTextWidth + 3, tooltipY - 3, tooltipX + tooltipTextWidth + 4, tooltipY + tooltipHeight + 3, backgroundColor, backgroundColor);
+            int borderColorStart = 0x505000FF;
+            int borderColorEnd = (borderColorStart & 0xFEFEFE) >> 1 | borderColorStart & 0xFF000000;
+            GuiUtils.drawGradientRect(zLevel, tooltipX - 3, tooltipY - 3 + 1, tooltipX - 3 + 1, tooltipY + tooltipHeight + 3 - 1, borderColorStart, borderColorEnd);
+            GuiUtils.drawGradientRect(zLevel, tooltipX + tooltipTextWidth + 2, tooltipY - 3 + 1, tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 3 - 1, borderColorStart, borderColorEnd);
+            GuiUtils.drawGradientRect(zLevel, tooltipX - 3, tooltipY - 3, tooltipX + tooltipTextWidth + 3, tooltipY - 3 + 1, borderColorStart, borderColorStart);
+            GuiUtils.drawGradientRect(zLevel, tooltipX - 3, tooltipY + tooltipHeight + 2, tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 3, borderColorEnd, borderColorEnd);
+
+            for (int lineNumber = 0; lineNumber < textLines.size(); ++lineNumber) {
+                String line = textLines.get(lineNumber);
+                font.drawStringWithShadow(line, (float) tooltipX, (float) tooltipY, -1);
+
+                if (lineNumber + 1 == titleLinesCount) {
+                    tooltipY += 2;
+                }
+
+                tooltipY += 10;
+            }
+
+            GlStateManager.enableLighting();
+            GlStateManager.enableDepth();
+            RenderHelper.enableStandardItemLighting();
+            GlStateManager.enableRescaleNormal();
+            GlStateManager.popMatrix();
+        }
     }
 }
