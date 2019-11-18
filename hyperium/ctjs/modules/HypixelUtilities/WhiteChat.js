fd3bc73c231f659ed175aac372f7246ec6edc05e
asbyth <cyronize@gmail.com>
2019-11-18 16:04:41 -0500

    commit unexist

--- hyperium/ctjs/modules/HypixelUtilities/WhiteChat.js
+++
@@ -1,8 +0,0 @@
-register("chat", function(name, message, event) {
-	if (!HypixelUtilities.getSetting("CleanChat", "AllWhiteChat")) return;
-
-	if (name.endsWith("&r&7")) {
-		event.setCanceled(true);
-		ChatLib.chat(name + "&r&f: " + ChatLib.removeFormatting(message));
-	}
-}).setCriteria("&r${name}: ${message}&r");
--- hyperium/ctjs/modules/HypixelUtilities/autoFriend.js
+++
@@ -1,5 +0,0 @@
-register("chat", function(name) {
-	if (!HypixelUtilities.getSetting("HypixelFriends", "auto accept")) return;
-
-	ChatLib.say("/f accept " + ChatLib.removeFormatting(name).replace(/\[.+]/g, "").trim());
-}).setCriteria("&r&eFriend request from &r${name}&r&9\n&r&a&l").setParameter("contains");
