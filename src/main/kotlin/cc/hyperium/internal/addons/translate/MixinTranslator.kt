41023c9d1f9e0ef9feb4d723269511d84aec04e3
asbyth <cyronize@gmail.com>
2019-11-18 22:27:37 -0500

    its ignored for whatever reason?

---
+++ src/main/kotlin/cc/hyperium/internal/addons/translate/MixinTranslator.kt
@@ -0,0 +1,12 @@
+package cc.hyperium.internal.addons.translate
+
+import cc.hyperium.internal.addons.AddonManifest
+import org.spongepowered.asm.mixin.Mixins
+
+class MixinTranslator : AbstractTranslator() {
+    override fun translate(manifest: AddonManifest) {
+        manifest.mixinConfigs?.forEach {
+            Mixins.addConfiguration(it)
+        }
+    }
+}
