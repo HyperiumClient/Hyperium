564a7e0788404af1bf3ae2d709ae59b6c8a68dbc
asbyth <cyronize@gmail.com>
2019-11-18 19:49:02 -0500

    stop the client from sending update notif if world is null or version is -1

--- src/main/java/cc/hyperium/internal/UpdateChecker.java
+++ src/main/java/cc/hyperium/internal/UpdateChecker.java
@@ -7,6 +7,7 @@ import cc.hyperium.event.network.server.ServerJoinEvent;
 import cc.hyperium.mods.sk1ercommon.Multithreading;
 import cc.hyperium.utils.ChatColor;
 import cc.hyperium.utils.UpdateUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.event.ClickEvent;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
@@ -38,24 +39,27 @@ public class UpdateChecker {
             boolean beta = updateUtils.isBeta();
             int version = Metadata.getVersionID();
             Hyperium.LOGGER.info("version is: " + version);
+            if (version == -1) return; // dev
             if (beta) return; // dont alert beta users
             if (latest) return; //If they're on the latest version, I don't want to mess with them.
 
-            Hyperium.INSTANCE.getNotification().display("You have an update pending.",
-                "Click here to be sent to the installer.",
-                10f,
-                null, () -> {
-                    try {
-                        Desktop.getDesktop().browse(new URI("https://hyperium.cc/downloads"));
-                    } catch (IOException | URISyntaxException e) {
-                        IChatComponent urlComponent = new
-                            ChatComponentText(ChatColor.RED + "[Hyperium] " +
-                            ChatColor.GRAY + "Click to be sent to update Hyperium");
-                        urlComponent.getChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://hyperium.cc/downloads"));
-                        Hyperium.INSTANCE.getHandlers().getGeneralChatHandler().sendMessage(urlComponent);
-                    }
-                }, new Color(200, 150, 50));
-            asked = true;
+            if (Minecraft.getMinecraft().theWorld != null) {
+                Hyperium.INSTANCE.getNotification().display("You have an update pending.",
+                        "Click here to be sent to the installer.",
+                        10f,
+                        null, () -> {
+                            try {
+                                Desktop.getDesktop().browse(new URI("https://hyperium.cc/downloads"));
+                            } catch (IOException | URISyntaxException e) {
+                                IChatComponent urlComponent = new
+                                        ChatComponentText(ChatColor.RED + "[Hyperium] " +
+                                        ChatColor.GRAY + "Click to be sent to update Hyperium");
+                                urlComponent.getChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://hyperium.cc/downloads"));
+                                Hyperium.INSTANCE.getHandlers().getGeneralChatHandler().sendMessage(urlComponent);
+                            }
+                        }, new Color(200, 150, 50));
+                asked = true;
+            }
         });
     }
 }
