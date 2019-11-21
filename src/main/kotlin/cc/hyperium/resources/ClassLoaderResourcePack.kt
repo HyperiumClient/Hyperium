1bf83b5a9a99efffdcefbfffec575228864f4901
AmpTheDev <gerritegberink3@gmail.com>
2019-11-21 22:01:26 +0100

    Basic prod works (OF not supported yet)

--- src/main/kotlin/cc/hyperium/resources/ClassLoaderResourcePack.kt
+++ src/main/kotlin/cc/hyperium/resources/ClassLoaderResourcePack.kt
@@ -1,51 +1,17 @@
 package cc.hyperium.resources
 
-import net.minecraft.client.renderer.texture.TextureUtil
 import net.minecraft.client.resources.AbstractResourcePack
-import net.minecraft.client.resources.IResourcePack
-import net.minecraft.client.resources.data.IMetadataSection
-import net.minecraft.client.resources.data.IMetadataSerializer
-import net.minecraft.util.ResourceLocation
-import java.awt.image.BufferedImage
-import java.io.FileNotFoundException
+import java.io.File
 import java.io.InputStream
-import java.lang.IllegalArgumentException
 
-class ClassLoaderResourcePack(private val domain: String) : IResourcePack {
+class ClassLoaderResourcePack(domain: String) : AbstractResourcePack(File("ClassLoaderResourcePack:$domain")) {
+    override fun getInputStreamByName(name: String?): InputStream? {
+        return if (name == null) null else javaClass.getResourceAsStream("/$name")
+    }
+
+    override fun hasResourceName(name: String?) = getInputStreamByName(name) != null
 
     private val domainSet = setOf(domain)
 
-    override fun getInputStream(location: ResourceLocation?) =
-        getResourceStream(location) ?: throw FileNotFoundException(location?.resourcePath)
-
-    private fun getResourceStream(location: ResourceLocation?): InputStream? {
-        val nLocation = location ?: throw IllegalArgumentException("Resource location may not be null")
-        return javaClass.getResourceAsStream("/assets/${nLocation.resourceDomain}/${nLocation.resourcePath}")
-    }
-
-    override fun resourceExists(location: ResourceLocation?) = getResourceStream(location) != null
-
     override fun getResourceDomains() = domainSet
-
-    override fun <T : IMetadataSection?> getPackMetadata(
-        metadataSerializer: IMetadataSerializer?,
-        metadataSectionName: String?
-    ): T? {
-        return try {
-            AbstractResourcePack.readMetadataPublic<T>(
-                metadataSerializer,
-                javaClass.getResourceAsStream("/pack.mcmeta"),
-                metadataSectionName
-            )
-        } catch (ex: RuntimeException) {
-            null
-        } catch (ex: FileNotFoundException) {
-            null
-        }
-    }
-
-    override fun getPackImage(): BufferedImage =
-        TextureUtil.readBufferedImage(javaClass.getResourceAsStream("/pack.png"))
-
-    override fun getPackName() = "ClassLoaderResourcePack:$domain"
 }
