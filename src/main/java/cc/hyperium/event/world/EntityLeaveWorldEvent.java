9fd116473aa0f325a0985b038d12bf6aa5a45794
asbyth <cyronize@gmail.com>
2019-11-23 10:30:32 -0500

    add event for entity leaving

---
+++ src/main/java/cc/hyperium/event/world/EntityLeaveWorldEvent.java
@@ -0,0 +1,24 @@
+package cc.hyperium.event.world;
+
+import cc.hyperium.event.Event;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+
+public class EntityLeaveWorldEvent extends Event {
+
+    private final World world;
+    private final Entity entity;
+
+    public EntityLeaveWorldEvent(World world, Entity entity) {
+        this.world = world;
+        this.entity = entity;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+}
