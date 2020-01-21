8e54e14467b6e8271b93f0ca5a0d46568d013328
lego3708 <l3gomindstorms@gmail.com>
2020-01-21 18:21:28 +0000

    Added Smooth Chat by LlamaLad7#7444

---
+++ src/main/java/cc/hyperium/handlers/handlers/chat/ChatAnimationHandler.java
@@ -0,0 +1,29 @@
+/*
+ *       Copyright (C) 2018-present Hyperium <https://hyperium.cc/>
+ *
+ *       This program is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Lesser General Public License as published
+ *       by the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Lesser General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Lesser General Public License
+ *       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cc.hyperium.handlers.handlers.chat;
+
+import cc.hyperium.event.InvokeEvent;
+import cc.hyperium.event.client.TickEvent;
+import net.minecraft.client.gui.GuiNewChat;
+
+public class ChatAnimationHandler {
+    //Made by LlamaLad7#7444
+    @InvokeEvent
+    public void clientTickEvent(TickEvent event) {
+        if (GuiNewChat.ticksLeft > 0) GuiNewChat.ticksLeft--;
+    }
+}
