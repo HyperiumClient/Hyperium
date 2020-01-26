137accbeafd5b00f88f6de88f1646fdc76f90e24
asbyth <cyronize@gmail.com>
2020-01-25 21:30:45 -0500

    add more to debug

--- src/main/java/cc/hyperium/debug/HyperiumDebug.java
+++ src/main/java/cc/hyperium/debug/HyperiumDebug.java
@@ -4,7 +4,8 @@ import cc.hyperium.event.EventBus;
 import cc.hyperium.event.InvokeEvent;
 import cc.hyperium.event.render.RenderHUDEvent;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiOverlayDebug;
+import net.minecraft.client.renderer.OpenGlHelper;
+import org.lwjgl.opengl.GL11;
 
 public class HyperiumDebug {
 
@@ -28,5 +29,10 @@ public class HyperiumDebug {
         long totalMemory = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / mbDiv;
         mc.fontRendererObj.drawStringWithShadow("fps: " + Minecraft.getDebugFPS(), 3, 3, -1);
         mc.fontRendererObj.drawStringWithShadow("memory usage: " + totalMemory + " / " + maxMemory + "MB", 3, 13, -1);
+        mc.fontRendererObj.drawStringWithShadow("supported opengl ver: " + GL11.glGetString(GL11.GL_VERSION), 3, 23, -1);
+        mc.fontRendererObj.drawStringWithShadow("cpu: " + OpenGlHelper.getCpu(), 3, 33, -1);
+        mc.fontRendererObj.drawStringWithShadow("gpu: " + GL11.glGetString(GL11.GL_RENDERER), 3, 43, -1);
+        mc.fontRendererObj.drawStringWithShadow("operating system: " + System.getProperty("os.name"), 3, 53, -1);
+        mc.fontRendererObj.drawStringWithShadow("architecture type: " + System.getProperty("os.arch"), 3, 63, -1);
     }
 }
