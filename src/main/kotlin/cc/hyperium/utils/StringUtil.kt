fe7fbe3a89bb8936c568560650263116d7fefce4
asbyth <cyronize@gmail.com>
2020-02-02 15:48:17 -0500

    optimize chroma wave string a bit

--- src/main/kotlin/cc/hyperium/utils/StringUtil.kt
+++ src/main/kotlin/cc/hyperium/utils/StringUtil.kt
@@ -8,21 +8,18 @@ object StringUtil {
     fun drawChromaWaveString(text: String, xIn: Int, y: Int) {
         val renderer = Minecraft.getMinecraft().fontRendererObj
         var x = xIn
-        for (c in text.toCharArray()) {
-            val dif = x * 10 - (y * 10).toLong()
-            val l = System.currentTimeMillis() - dif
-            val ff = 2000.0f
-            val i = Color.HSBtoRGB((l % ff.toInt()).toFloat() / ff, 0.8f, 0.8f)
-            val tmp = c.toString()
+        for (chars in text.toCharArray()) {
+            val dif = x * 10 - (y * 10)
+            val color = Color.HSBtoRGB((System.currentTimeMillis() - dif % 2000f / 2000f), 0.8f, 0.8f)
             renderer.drawString(
-                tmp,
-                x.toDouble().toFloat(),
-                y.toDouble().toFloat(),
-                i,
+                chars.toString(),
+                x.toFloat(),
+                y.toFloat(),
+                color,
                 false
             )
 
-            x += renderer.getCharWidth(c)
+            x += renderer.getCharWidth(chars)
         }
     }
 }
