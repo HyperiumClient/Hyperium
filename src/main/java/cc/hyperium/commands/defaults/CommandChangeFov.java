1b39d43bf55209e2b9c295383ee780dfca48af76
AmpTheDev <gerritegberink3@gmail.com>
2020-02-05 08:32:16 +0100

    version json building

--- src/main/java/cc/hyperium/commands/defaults/CommandChangeFov.java
+++ src/main/java/cc/hyperium/commands/defaults/CommandChangeFov.java
@@ -34,11 +34,18 @@ public class CommandChangeFov implements BaseCommand {
             sendPlayerMessage("Too little arguments. Usage: /fov <number>");
         } else if (args[0].equals("0")) {
             sendPlayerMessage("Changing your fov to 0 is disabled due to breaking the game.");
-        } else if (!NumberUtils.isParsable(args[0])) {
-            sendPlayerMessage("You cannot use a letter.");
         } else {
-            sendPlayerMessage("Fov changed from &e" + Minecraft.getMinecraft().gameSettings.fovSetting + " &rto &a" + Float.parseFloat(args[0]));
-            Minecraft.getMinecraft().gameSettings.fovSetting = Float.parseFloat(args[0]);
+            try {
+                float f = Float.parseFloat(args[0]);
+                if (f == 0) { // precision errors can make the parsed float 0 while the string isn't
+                    sendPlayerMessage("Changing your fov to 0 is disabled due to breaking the game.");
+                    return;
+                }
+                sendPlayerMessage("Fov changed from &e" + Minecraft.getMinecraft().gameSettings.fovSetting + " &rto &a" + f);
+                Minecraft.getMinecraft().gameSettings.fovSetting = f;
+            } catch (NumberFormatException e) {
+                sendPlayerMessage("You cannot use a letter.");
+            }
         }
     }
 }
