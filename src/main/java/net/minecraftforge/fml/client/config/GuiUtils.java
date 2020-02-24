4afb76083c05bb5ae31f0332b16915c418002488
asbyth <cyronize@gmail.com>
2020-02-24 15:01:27 -0500

    fix our copyright overwriting forge's

--- src/main/java/net/minecraftforge/fml/client/config/GuiUtils.java
+++ src/main/java/net/minecraftforge/fml/client/config/GuiUtils.java
@@ -1,18 +1,13 @@
 /*
- *     Copyright (C) 2018  Hyperium <https://hyperium.cc/>
+ * Forge Mod Loader
+ * Copyright (c) 2012-2014 cpw.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published
- *     by the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Contributors (this class):
+ *     bspkrs - implementation
  */
 
 package net.minecraftforge.fml.client.config;
