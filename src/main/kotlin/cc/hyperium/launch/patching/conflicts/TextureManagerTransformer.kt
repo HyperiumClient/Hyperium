b4c1c8600c73257df5529f78bd5fb9e30eeb5ad8
asbyth <cyronize@gmail.com>
2020-04-02 12:27:40 -0400

    remove white space

--- src/main/kotlin/cc/hyperium/launch/patching/conflicts/TextureManagerTransformer.kt
+++ src/main/kotlin/cc/hyperium/launch/patching/conflicts/TextureManagerTransformer.kt
@@ -15,7 +15,8 @@ class TextureManagerTransformer : ConflictTransformer {
     override fun getClassName() = "bmj"
 
     override fun transform(original: ClassNode): ClassNode {
-        original.fields.find { it.name == "mapTextureObjects"
+        original.fields.find {
+            it.name == "mapTextureObjects"
         }?.apply {
             access = Opcodes.ACC_PUBLIC or Opcodes.ACC_FINAL
         }
